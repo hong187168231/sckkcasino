4d9909b38cbf03565b00340ee4f4b116d1f26a3d
tom01 <941707835@qq.com>
2021-09-04 19:54:41 +0800

    添加权限相关依赖

---
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/inteceptor/AuthenticationInteceptor.java
@@ -0,0 +1,22 @@
+package com.qianyi.casinoadmin.inteceptor;
+
+import com.qianyi.casinocore.utils.LoginUtil;
+import com.qianyi.modulecommon.inteceptor.AbstractAuthenticationInteceptor;
+import com.qianyi.modulejjwt.JjwtUtil;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class AuthenticationInteceptor extends AbstractAuthenticationInteceptor {
+
+    @Override
+    public boolean hasPermission(HttpServletRequest request) {
+        String token = LoginUtil.getToken();
+
+        if(JjwtUtil.check(token)){
+            return true;
+        }
+        return false;
+    }
+}
