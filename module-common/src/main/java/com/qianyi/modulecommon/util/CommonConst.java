c6328b7666aeaa6229eb6c6c2971dd2f8b3c5261
zhenhao-java <zhenhao.java@gmail.com>
2021-09-06 22:15:00 +0800

    新增常量类

---
+++ module-common/src/main/java/com/qianyi/modulecommon/util/CommonConst.java
@@ -0,0 +1,58 @@
+package com.qianyi.modulecommon.util;
+
+/**
+ 公共常量类
+ */
+public class CommonConst {
+    /**
+     * 连字符号("-")
+     */
+    public static final String HYPHEN = "-";
+    /**
+     * 连字符号("_")
+     */
+    public static final String UNDERLINE_SYMBOL = "_";
+    /**
+     * 点分割符
+     */
+    public static final String POINT_SPLIT = ".";
+    /**
+     * '|'分割符
+     */
+    public static final String VERTICAL_SPLIT = "|";
+    /**
+     * '|'分割符
+     */
+    public static final String VERTI_SPLIT = "[|]";
+    /**
+     * 空符号
+     */
+    public static final String BLANK_SYMBOL = "";
+    /**
+     * 空格字符号(" ")
+     */
+    public static final String SPACE_SPLIT = " ";
+    /**
+     * 冒号字符号(":")
+     */
+    public static final String COLON_SPLIT = ":";
+    /**
+     * 等于号字符号("=")
+     */
+    public static final String EQUALS_SPLIT = "=";
+
+    public static final String AT_SPLIT="@";
+    public static final int NUMBER_1 = 1;
+    public static final int NUMBER_0 = 0;
+    public static final int NUMBER_2 = 2;
+    public static final int NUMBER_3 = 3;
+    public static final int NUMBER_4 = 4;
+    public static final int NUMBER_5 = 5;
+    public static final int NUMBER_6 = 6;
+    public static final int NUMBER_7 = 7;
+    public static final int NUMBER_8 = 8;
+    public static final int NUMBER_9 = 9;
+    public static final int NUMBER_10 = 10;
+    public static final String SUCCESS = "SUCCESS";
+
+}
