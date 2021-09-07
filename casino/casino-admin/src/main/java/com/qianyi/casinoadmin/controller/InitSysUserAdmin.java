1e30dd7d11b23457770d4cc4b8eed308e7aec830
tom01 <941707835@qq.com>
2021-09-07 21:27:40 +0800

    初始化超级管理员admin

---
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/InitSysUserAdmin.java
@@ -0,0 +1,47 @@
+package com.qianyi.casinoadmin.controller;
+
+import com.qianyi.casinoadmin.util.LoginUtil;
+import com.qianyi.casinocore.model.SysUser;
+import com.qianyi.casinocore.service.SysUserService;
+import com.qianyi.modulecommon.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class InitSysUserAdmin {
+    /**
+     * application没有配置，就给一个默认值
+     */
+    @Value("${project.username:admin}")
+    public String userName;
+
+    @Value("${project.password:123456}")
+    public String password;
+
+    @Autowired
+    private SysUserService sysUserService;
+
+    /**
+     * 初始化，添加超级管理员
+     */
+    @PostConstruct
+    public void initSuperSysUser(){
+        if(LoginUtil.checkNull(userName, password)){
+            return;
+        }
+        SysUser sys = sysUserService.findByUserName(userName);
+        if(sys != null){
+            return;
+        }
+        SysUser sysUser = new SysUser();
+        sysUser.setUserFlag(Constants.open);
+        sysUser.setUserName(userName);
+        String bcryptPassword = LoginUtil.bcrypt(password);
+        sysUser.setPassWord(bcryptPassword);
+        sysUser.setNickName(userName);
+        sysUserService.save(sysUser);
+    }
+}
--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/LoginController.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/LoginController.java
@@ -23,6 +23,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 
 /**
  * 用户谷歌验证登录
