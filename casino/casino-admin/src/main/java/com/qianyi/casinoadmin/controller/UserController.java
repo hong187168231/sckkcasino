409f3b7fe1d083e3a66b7b2e9ea4582c2c836127
tom01 <941707835@qq.com>
2021-09-07 16:30:40 +0800

    用户中心增删改查

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/UserController.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/UserController.java
@@ -157,7 +157,7 @@ public class UserController {
     public ResponseEntity deteleUser(Long id){
         User user = userService.findById(id);
         if(user == null){
-            ResponseUtil.fail();
+            ResponseUtil.custom("账户不存在");
         }
         userService.deleteById(id);
         return ResponseUtil.success();
