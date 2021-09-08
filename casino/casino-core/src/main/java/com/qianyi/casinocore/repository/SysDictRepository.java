7ff79102da4e90bc4fd7b7abb15c295cc6e41a99
tom01 <941707835@qq.com>
2021-09-08 22:15:49 +0800

    注释代码

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/SysDictRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/SysDictRepository.java
@@ -1,6 +1,6 @@
-package com.qianyi.casinocore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SysDictRepository extends JpaRepository<SysDict,Long> {
-}
+//package com.qianyi.casinocore.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface SysDictRepository extends JpaRepository<SysDict,Long> {
+//}
