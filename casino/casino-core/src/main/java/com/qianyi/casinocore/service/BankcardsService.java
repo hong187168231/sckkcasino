66a4b577ec9f6f42c20f4c22d07f7459d911e0bf
kate <kate98765@gmail.com>
2021-09-08 22:11:51 +0800

    注释代码

--- casino/casino-core/src/main/java/com/qianyi/casinocore/service/BankcardsService.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/service/BankcardsService.java
@@ -1,75 +1,75 @@
-package com.qianyi.casinocore.service;
-
-import com.qianyi.casinocore.model.Bankcards;
-import com.qianyi.casinocore.repository.BankcardsRepository;
-import com.qianyi.modulecommon.util.CommonUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class BankcardsService {
-
-    @Autowired
-    BankcardsRepository bankcardsRepository;
-
-    public List<Bankcards> findBankcardsByUserId(Long userId){
-        return bankcardsRepository.findBankcardsByUserIdOrderByDefaultCardDesc(userId);
-    }
-
-    public Bankcards boundCard(Bankcards bankcards){
-        return bankcardsRepository.save(bankcards);
-    }
-
-    public Bankcards findBankCardsInByUserId(Long userId) {
-        return bankcardsRepository.findFirstByUserId(userId);
-    }
-
-    public int countByUserId(Long userId){
-        return bankcardsRepository.countByUserId(userId);
-    }
-
-
-    /**
-     * 查询用户银行卡
-     *
-     * @param bankcards
-     * @return
-     */
-    public List<Bankcards> findUserBank(Bankcards bankcards) {
-        Specification<Bankcards> condition = this.getCondition(bankcards);
-        List<Bankcards> bankcardsList = bankcardsRepository.findAll(condition);
-        return bankcardsList;
-    }
-
-    private Specification<Bankcards> getCondition(Bankcards bankcards) {
-        Specification<Bankcards> specification = new Specification<Bankcards>(){
-            @Override
-            public Predicate toPredicate(Root<Bankcards> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
-                List<Predicate> list = new ArrayList<Predicate>();
-                if(bankcards.getUserId() != null){
-                    list.add(cb.equal(root.get("userId").as(Long.class), bankcards.getUserId()));
-                }
-                if(bankcards.getBankId() != null){
-                    list.add(cb.equal(root.get("bankId").as(Long.class), bankcards.getBankId()));
-                }
-                if(CommonUtil.checkNull(bankcards.getRealName())){
-                    list.add(cb.equal(root.get("realName").as(String.class), bankcards.getRealName()));
-                }
-                if(CommonUtil.checkNull(bankcards.getBankAccount())){
-                    list.add(cb.equal(root.get("bankAccount").as(String.class), bankcards.getBankAccount()));
-                }
-                return cb.and(list.toArray(new Predicate[list.size()]));
-            }
-        };
-        return specification;
-    }
-}
+//package com.qianyi.casinocore.service;
+//
+//import com.qianyi.casinocore.model.Bankcards;
+//import com.qianyi.casinocore.repository.BankcardsRepository;
+//import com.qianyi.modulecommon.util.CommonUtil;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.jpa.domain.Specification;
+//import org.springframework.stereotype.Service;
+//import org.springframework.util.StringUtils;
+//
+//import javax.persistence.criteria.CriteriaBuilder;
+//import javax.persistence.criteria.CriteriaQuery;
+//import javax.persistence.criteria.Predicate;
+//import javax.persistence.criteria.Root;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service
+//public class BankcardsService {
+//
+//    @Autowired
+//    BankcardsRepository bankcardsRepository;
+//
+//    public List<Bankcards> findBankcardsByUserId(Long userId){
+//        return bankcardsRepository.findBankcardsByUserIdOrderByDefaultCardDesc(userId);
+//    }
+//
+//    public Bankcards boundCard(Bankcards bankcards){
+//        return bankcardsRepository.save(bankcards);
+//    }
+//
+//    public Bankcards findBankCardsInByUserId(Long userId) {
+//        return bankcardsRepository.findFirstByUserId(userId);
+//    }
+//
+//    public int countByUserId(Long userId){
+//        return bankcardsRepository.countByUserId(userId);
+//    }
+//
+//
+//    /**
+//     * 查询用户银行卡
+//     *
+//     * @param bankcards
+//     * @return
+//     */
+//    public List<Bankcards> findUserBank(Bankcards bankcards) {
+//        Specification<Bankcards> condition = this.getCondition(bankcards);
+//        List<Bankcards> bankcardsList = bankcardsRepository.findAll(condition);
+//        return bankcardsList;
+//    }
+//
+//    private Specification<Bankcards> getCondition(Bankcards bankcards) {
+//        Specification<Bankcards> specification = new Specification<Bankcards>(){
+//            @Override
+//            public Predicate toPredicate(Root<Bankcards> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+//                List<Predicate> list = new ArrayList<Predicate>();
+//                if(bankcards.getUserId() != null){
+//                    list.add(cb.equal(root.get("userId").as(Long.class), bankcards.getUserId()));
+//                }
+//                if(bankcards.getBankId() != null){
+//                    list.add(cb.equal(root.get("bankId").as(Long.class), bankcards.getBankId()));
+//                }
+//                if(CommonUtil.checkNull(bankcards.getRealName())){
+//                    list.add(cb.equal(root.get("realName").as(String.class), bankcards.getRealName()));
+//                }
+//                if(CommonUtil.checkNull(bankcards.getBankAccount())){
+//                    list.add(cb.equal(root.get("bankAccount").as(String.class), bankcards.getBankAccount()));
+//                }
+//                return cb.and(list.toArray(new Predicate[list.size()]));
+//            }
+//        };
+//        return specification;
+//    }
+//}
