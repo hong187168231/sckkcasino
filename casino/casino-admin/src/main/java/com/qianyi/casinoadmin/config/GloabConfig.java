3a521415496a5fbcd49f9a9b6d0149825325ca75
tom01 <941707835@qq.com>
2021-09-10 15:54:29 +0800

    NEW拦截器改成注入

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/config/GloabConfig.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/config/GloabConfig.java
@@ -2,6 +2,7 @@ package com.qianyi.casinoadmin.config;
 
 import com.qianyi.casinoadmin.inteceptor.AuthenticationInteceptor;
 import com.qianyi.modulecommon.util.CommonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -28,9 +29,12 @@ public class GloabConfig implements WebMvcConfigurer {
                 .exposedHeaders("*");
     }
 
+    @Autowired
+    AuthenticationInteceptor authenticationInteceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthenticationInteceptor()).addPathPatterns("/**")
+        registry.addInterceptor(authenticationInteceptor).addPathPatterns("/**")
                 .excludePathPatterns("/js/**")
                 .excludePathPatterns("/swagger**/**")
                 .excludePathPatterns("/webjars/**")
