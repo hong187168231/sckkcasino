6f26a28a09451305584b499c85315392daa68766
tom01 <941707835@qq.com>
2021-09-10 15:44:22 +0800

    封号拦截SysUser表管理用户

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/inteceptor/AuthenticationInteceptor.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/inteceptor/AuthenticationInteceptor.java
@@ -1,14 +1,30 @@
 package com.qianyi.casinoadmin.inteceptor;
 
 import com.qianyi.casinoadmin.util.LoginUtil;
+import com.qianyi.casinocore.model.SysUser;
+import com.qianyi.casinocore.model.User;
+import com.qianyi.casinocore.service.SysUserService;
+import com.qianyi.casinocore.service.UserService;
 import com.qianyi.modulecommon.inteceptor.AbstractAuthenticationInteceptor;
 import com.qianyi.modulejjwt.JjwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class AuthenticationInteceptor extends AbstractAuthenticationInteceptor {
+
+    @Autowired
+    SysUserService sysUserService;
+
+    @Override
+    protected boolean hasBan() {
+        Long authId=LoginUtil.getLoginUserId();
+        SysUser user=sysUserService.findById(authId);
+        boolean flag= SysUser.checkUser(user);
+        return !flag;
+    }
 
     @Override
     public boolean hasPermission(HttpServletRequest request) {
