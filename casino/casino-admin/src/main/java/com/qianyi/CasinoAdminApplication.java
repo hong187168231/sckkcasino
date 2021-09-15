5066fa41b074512a86141678274075178667cc92
zhenhao-java <zhenhao.java@gmail.com>
2021-09-15 20:49:04 +0800

    新增管理后台清除超时充值订单定时任务

--- casino/casino-admin/src/main/java/com/qianyi/CasinoAdminApplication.java
+++ casino/casino-admin/src/main/java/com/qianyi/CasinoAdminApplication.java
@@ -2,8 +2,10 @@ package com.qianyi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CasinoAdminApplication {
 
     public static void main(String[] args) {
--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/ChargeOrderRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/ChargeOrderRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ChargeOrderRepository extends JpaRepository<ChargeOrder,Long>, JpaSpecificationExecutor<ChargeOrder> {
     @Query(value = "select * from charge_order c where c.id = ? for update",nativeQuery = true)
     ChargeOrder findChargeOrderByIdUseLock(Long id);
+
+    @Query(value = "select * from charge_order c where c.status = ?1 and c.create_time <=?2 for update",nativeQuery = true)
+    List<ChargeOrder> findChargeOrdersUseLock(Integer status,String time);
 }
