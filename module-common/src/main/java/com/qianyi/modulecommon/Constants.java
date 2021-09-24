44bc5f0d49f6f4b4abdc2232fab23c4c893a521c
benson <liyongqi@0913>
2021-09-24 15:10:47 +0800

    注册ip账号数量限制改成卫语句格式,常量值修改

--- module-common/src/main/java/com/qianyi/modulecommon/Constants.java
+++ module-common/src/main/java/com/qianyi/modulecommon/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     //是/否，开/关, 真/假
     public final static Integer open = 1;
     public final static Integer close = 0;
-    public final static Integer yes = 1;
+    public final static Integer yes = 0;
     public final static Integer no = 1;
 
     //订单状态
