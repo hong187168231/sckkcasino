2af728a9d76faf8094a94689621c27197e9af652
tom01 <941707835@qq.com>
2021-09-25 16:45:38 +0800

    添加配置表

---
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/SysConfig.java
@@ -0,0 +1,42 @@
+package com.qianyi.casinocore.model;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+/**
+ * 此处不要继承BaseEntity
+ * 配置表等，后续可以用elk来做操作人记录追踪
+ */
+@Data
+@Entity
+@ApiModel("配置表")
+public class SysConfig {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 组别，根据组别判断是什么类型的值
+     */
+    private Integer sysGroup;
+
+    /**
+     * 配置名
+     */
+    private String name;
+
+    /**
+     * 配置值
+     * 不可以用json数据
+     */
+    private String value;
+
+    /**
+     * 备注
+     */
+    private String remark;
+}
