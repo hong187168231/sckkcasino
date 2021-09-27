c5ada83f86b705b9741d1a70c0685a02c878544f
benson <liyongqi@0913>
2021-09-27 19:17:37 +0800

    手动加白ip不再拉黑

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/inteceptor/IpLimitInteceptor.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/inteceptor/IpLimitInteceptor.java
@@ -28,13 +28,18 @@ public class IpLimitInteceptor extends AbstractIpLimitInteceptor {
         String ip = IpUtil.getIp(request);
         boolean access = redisLimitExcutor.tryAccess(ip);
         if(!access){
+            //ip已手动加白，不再拉黑
             IpBlack ipBlack = ipBlackService.findByIp(ip);
-            if (ipBlack == null) {
-                ipBlack = new IpBlack();
-                ipBlack.setIp(ip);
+            if (ipBlack != null) {
+                if (Constants.yes.equals(ipBlack.getStatus())) {
+                    return true;
+                }
+                return false;
             }
-            ipBlack.setStatus(Constants.no);
-            ipBlackService.save(ipBlack);
+            IpBlack ipBlack1 =new IpBlack();
+            ipBlack1.setIp(ip);
+            ipBlack1.setStatus(Constants.no);
+            ipBlackService.save(ipBlack1);
         }
         return access;
     }
