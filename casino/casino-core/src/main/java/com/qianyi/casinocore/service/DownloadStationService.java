c2d2d67036f956b189be51bf5836cee0f8e6b06c
benson <liyongqi@0913>
2021-09-28 19:32:40 +0800

    查询APP最新版本接口

--- casino/casino-core/src/main/java/com/qianyi/casinocore/service/DownloadStationService.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/service/DownloadStationService.java
@@ -1,5 +1,6 @@
 package com.qianyi.casinocore.service;
 
+import com.qianyi.casinocore.model.DownloadStation;
 import com.qianyi.casinocore.repository.DownloadStationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,4 +13,23 @@ public class DownloadStationService {
 
     @Autowired
     private DownloadStationRepository downloadStationRepository;
+
+    /**
+     * 根据终端类型查询最新版本
+     * @param terminalType
+     * @return
+     */
+    public DownloadStation getNewestVersion(Integer terminalType) {
+        return downloadStationRepository.findFirstByTerminalTypeOrderByCreateTimeDesc(terminalType);
+    }
+
+    /**
+     * 根据终端类型查询最新强制更新版本
+     * @param terminalType
+     * @param isForced
+     * @return
+     */
+    public DownloadStation getForcedNewestVersion(Integer terminalType,Integer isForced) {
+        return downloadStationRepository.findFirstByTerminalTypeAndIsForcedOrderByCreateTimeDesc(terminalType,isForced);
+    }
 }
