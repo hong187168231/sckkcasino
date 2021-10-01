3b947059adfe78a6a42ce33d5ec21462ec8a564d
zhenhao-java <zhenhao.java@gmail.com>
2021-10-01 14:39:45 +0800

    账变类型增加

--- casino/casino-core/src/main/java/com/qianyi/casinocore/enums/AccountChangeEnum.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/enums/AccountChangeEnum.java
@@ -5,7 +5,12 @@ package com.qianyi.casinocore.enums;
  */
 public enum AccountChangeEnum {
 
-    WASH_CODE(0, "XM","洗码领取");
+    WASH_CODE(0, "XM","洗码领取"),
+    TOPUP_CODE(1, "CZ","充值"),
+    ADD_CODE(2, "RGZJ","人工增加"),
+    WITHDRAW_CODE(3, "TX","提现"),
+    WITHDRAWDEFEATED_CODE(4, "TXSB","提现失败"),
+    SUB_CODE(5, "RGKJ","人工扣减");
 
     private Integer type;
 
