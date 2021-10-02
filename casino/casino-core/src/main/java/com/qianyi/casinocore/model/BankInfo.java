446304e1f696c8049fcd36c5550c8483952356a4
zhenhao-java <zhenhao.java@gmail.com>
2021-10-02 17:56:16 +0800

    去掉银行卡logo

--- casino/casino-core/src/main/java/com/qianyi/casinocore/model/BankInfo.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/BankInfo.java
@@ -18,8 +18,8 @@ public class BankInfo extends BaseEntity {
 	 */
 	@Column(unique = true)
 	private String bankName;
-
-	private String bankLogo;
+//
+//	private String bankLogo;
 
 	/** 银行类型：默认0:银行卡，1：支付宝，2：微信，3：QQ */
 	private Integer bankType;
