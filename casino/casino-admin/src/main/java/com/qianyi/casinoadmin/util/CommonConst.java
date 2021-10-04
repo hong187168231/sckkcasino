562d0dc3b4746d8bfc5642e655f6f82ef386e7a6
zhenhao-java <zhenhao.java@gmail.com>
2021-10-04 17:42:25 +0800

    银行卡校验

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/util/CommonConst.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/util/CommonConst.java
@@ -79,4 +79,6 @@ public class CommonConst {
     public static final String STRING_2 = "2";
     public static final String STRING_3 = "3";
     public static final String STRING_4 = "4";
+    //校验数字
+    public static final String regex = "^[0-9]*$";
 }
