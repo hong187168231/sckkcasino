0406d6f7637759f02be9bb2b6a6bccdb237978f8
kate <kate98765@gmail.com>
2021-10-07 15:07:33 +0800

    提交rabbitmq 代码

---
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/task/DirectConsumer.java
@@ -0,0 +1,24 @@
+package com.qianyi.casinoadmin.task;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@RabbitListener(queues = "TestDirectQueue")
+@Component
+public class DirectConsumer {
+
+    @RabbitHandler
+    public void process(Map map, Channel channel, Message message) throws IOException {
+        log.info("消费者接受到的消息是：{}",map.toString());
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+    }
+}
