fed95c12976922af8ed8787910b12611f55024c4
kate <kate98765@gmail.com>
2021-10-08 13:27:42 +0800

    提交分润代码

--- casino/casino-core/src/main/java/com/qianyi/casinocore/model/ProxyDayReport.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/ProxyDayReport.java
@@ -1,5 +1,6 @@
 package com.qianyi.casinocore.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,11 +10,17 @@ public class ProxyDayReport extends BaseEntity{
 
     private Long userId;
 
+    @ApiModelProperty("有效投注")
     private BigDecimal betAmount = BigDecimal.ZERO;
+    @ApiModelProperty("分润")
     private BigDecimal profitAmount= BigDecimal.ZERO;
+    @ApiModelProperty("今日新增")
     private Integer newNum;
+    @ApiModelProperty("团队人数")
     private Integer groupNum;
+    @ApiModelProperty("直属下级")
     private Integer directNum;
+    @ApiModelProperty("日期")
     private String dayTime;
 
 }
--- casino/casino-core/src/main/java/com/qianyi/casinocore/model/ProxyReport.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/ProxyReport.java
@@ -1,5 +1,6 @@
 package com.qianyi.casinocore.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,23 +10,38 @@ public class ProxyReport extends BaseEntity{
 
     private Long userId;
     //全部
+    @ApiModelProperty("分润")
     private BigDecimal allProfitAmount=BigDecimal.ZERO;
+    @ApiModelProperty("有效投注")
     private BigDecimal allBetAmount=BigDecimal.ZERO;
+    @ApiModelProperty("团队人数")
     private Integer allGroupNum;
+    @ApiModelProperty("投注人数")
     private Integer allBetNum;
+    @ApiModelProperty("充值人数")
     private Integer allChargeNum;
 
     //直属
+    @ApiModelProperty("分润")
     private BigDecimal directProfitAmount=BigDecimal.ZERO;
+    @ApiModelProperty("有效投注")
     private BigDecimal directBetAmount=BigDecimal.ZERO;
+    @ApiModelProperty("团队人数")
     private Integer directGroupNum;
+    @ApiModelProperty("投注人数")
     private Integer directBetNum;
+    @ApiModelProperty("充值人数")
     private Integer directChargeNum;
 
     //非直属
+    @ApiModelProperty("分润")
     private BigDecimal otherProfitAmount=BigDecimal.ZERO;
+    @ApiModelProperty("有效投注")
     private BigDecimal otherBetAmount=BigDecimal.ZERO;
+    @ApiModelProperty("团队人数")
     private Integer otherGroupNum;
+    @ApiModelProperty("投注人数")
     private Integer otherBetNum;
+    @ApiModelProperty("充值人数")
     private Integer otherChargeNum;
 }
