31ea81e804e66dccae64ba0ea947bc0373994dce
zhenhao-java <zhenhao.java@gmail.com>
2021-10-12 15:56:06 +0800

    报错处理。

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/ProxyUserRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/ProxyUserRepository.java
@@ -3,11 +3,14 @@ package com.qianyi.casinocore.repository;
 import com.qianyi.casinocore.model.ProxyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProxyUserRepository extends JpaRepository<ProxyUser,Long>, JpaSpecificationExecutor<ProxyUser> {
 
     ProxyUser findByUserName(String userName);
-
+    @Query("update ProxyUser u set u.gaKey=?2 where u.id=?1")
+    @Modifying
     void setSecretById(Long id, String gaKey);
 
     ProxyUser findAllById(Long id);
