64f27388c870a819bb0f7dc67b22a6e07dcaca3c
zhenhao-java <zhenhao.java@gmail.com>
2021-10-12 13:21:08 +0800

    三方注单添加我方会员账号

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/GameRecordController.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/GameRecordController.java
@@ -1,8 +1,12 @@
 package com.qianyi.casinoadmin.controller;
 
 import com.qianyi.casinoadmin.util.LoginUtil;
+import com.qianyi.casinoadmin.vo.GameRecordVo;
+import com.qianyi.casinoadmin.vo.PageResultVO;
 import com.qianyi.casinocore.model.GameRecord;
+import com.qianyi.casinocore.model.User;
 import com.qianyi.casinocore.service.GameRecordService;
+import com.qianyi.casinocore.service.UserService;
 import com.qianyi.modulecommon.reponse.ResponseEntity;
 import com.qianyi.modulecommon.reponse.ResponseUtil;
 import com.qianyi.modulecommon.util.CommonUtil;
@@ -25,7 +29,9 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/gameRecord")
@@ -33,6 +39,9 @@ import java.util.List;
 public class GameRecordController {
     @Autowired
     private GameRecordService gameRecordService;
+
+    @Autowired
+    private UserService userService;
     /**
      * 分页查询三方游戏注单
      *
@@ -47,17 +56,46 @@ public class GameRecordController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "pageSize", value = "每页大小(默认10条)", required = false),
             @ApiImplicitParam(name = "pageCode", value = "当前页(默认第一页)", required = false),
-            @ApiImplicitParam(name = "user", value = "会员账号", required = false),
+            @ApiImplicitParam(name = "user", value = "三方会员账号", required = false),
             @ApiImplicitParam(name = "betId", value = "注单号", required = false),
             @ApiImplicitParam(name = "gname", value = "游戏名称", required = false),
             @ApiImplicitParam(name = "gid", value = "游戏类型", required = false),
+            @ApiImplicitParam(name = "account", value = "我方会员账号", required = false),
     })
-    public ResponseEntity<GameRecord> findGameRecordPage(Integer pageSize, Integer pageCode, String user, String betId,String gname,Integer gid){
+    public ResponseEntity<GameRecord> findGameRecordPage(Integer pageSize, Integer pageCode, String user, String betId,
+                                                         String gname,Integer gid,String account){
         Sort sort = Sort.by("id").descending();
         Pageable pageable = LoginUtil.setPageable(pageCode, pageSize, sort);
-        Specification<GameRecord> condition = this.getCondition(user,betId,gname,gid);
+        Long userId = null;
+        if (!LoginUtil.checkNull(account)){
+            User byAccount = userService.findByAccount(account);
+            if (LoginUtil.checkNull(byAccount)){
+                return ResponseUtil.custom("用户不存在");
+            }
+            userId = byAccount.getId();
+        }
+        Specification<GameRecord> condition = this.getCondition(user,betId,gname,gid,userId);
         Page<GameRecord> gameRecordPage = gameRecordService.findGameRecordPage(condition, pageable);
-        return ResponseUtil.success(gameRecordPage);
+        PageResultVO<GameRecordVo> pageResultVO =new PageResultVO(gameRecordPage);
+        List<GameRecord> content = gameRecordPage.getContent();
+        if(content != null && content.size() > 0){
+            List<GameRecordVo> gameRecordVoList = new LinkedList<>();
+            List<Long> userIds = content.stream().map(GameRecord::getUserId).collect(Collectors.toList());
+            List<User> userList = userService.findAll(userIds);
+            if(userList != null){
+                content.stream().forEach(gameRecord ->{
+                    GameRecordVo gameRecordVo = new GameRecordVo(gameRecord);
+                    userList.stream().forEach(u->{
+                        if (u.getId().equals(gameRecord.getUserId())){
+                            gameRecordVo.setAccount(u.getAccount());
+                        }
+                    });
+                    gameRecordVoList.add(gameRecordVo);
+                });
+            }
+            pageResultVO.setContent(gameRecordVoList);
+        }
+        return ResponseUtil.success(pageResultVO);
     }
     /**
      * 统计三方游戏注单
@@ -84,7 +122,7 @@ public class GameRecordController {
      * @param
      * @return
      */
-    private Specification<GameRecord> getCondition(String user,String betId,String gname,Integer gid) {
+    private Specification<GameRecord> getCondition(String user,String betId,String gname,Integer gid,Long userId) {
         Specification<GameRecord> specification = new Specification<GameRecord>() {
             @Override
             public Predicate toPredicate(Root<GameRecord> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
@@ -101,6 +139,9 @@ public class GameRecordController {
                 }
                 if (gid != null) {
                     list.add(cb.equal(root.get("gid").as(Integer.class), gid));
+                }
+                if (userId != null) {
+                    list.add(cb.equal(root.get("userId").as(Long.class), userId));
                 }
                 predicate = cb.and(list.toArray(new Predicate[list.size()]));
                 return predicate;
