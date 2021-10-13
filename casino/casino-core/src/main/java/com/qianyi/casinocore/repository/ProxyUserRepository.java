6067aa472c3b775c9abf29184a8e52049b3d8488
tom01 <941707835@qq.com>
2021-10-13 18:38:38 +0800

    绑定谷歌验证码功能bug修改

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/ProxyUserRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/ProxyUserRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
+
 public interface ProxyUserRepository extends JpaRepository<ProxyUser,Long>, JpaSpecificationExecutor<ProxyUser> {
 
     ProxyUser findByUserName(String userName);
+
+    @Transactional
     @Query("update ProxyUser u set u.gaKey=?2 where u.id=?1")
     @Modifying
     void setSecretById(Long id, String gaKey);
