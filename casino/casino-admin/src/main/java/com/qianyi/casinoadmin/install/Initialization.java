daadb9590c4d91ba0fdb1ff5781be8f9ce82670d
zhenhao-java <zhenhao.java@gmail.com>
2021-10-15 22:36:10 +0800

    配置基础数据返佣

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/Initialization.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/Initialization.java
@@ -1,8 +1,12 @@
 package com.qianyi.casinoadmin.install;
 
+import com.qianyi.casinoadmin.install.file.PlatformConfigFile;
+import com.qianyi.casinoadmin.install.file.ProxyRebateConfigFile;
 import com.qianyi.casinoadmin.util.LoginUtil;
 import com.qianyi.casinocore.model.PlatformConfig;
+import com.qianyi.casinocore.model.ProxyRebateConfig;
 import com.qianyi.casinocore.service.PlatformConfigService;
+import com.qianyi.casinocore.service.ProxyRebateConfigService;
 import com.qianyi.casinocore.util.CommonConst;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,34 +19,60 @@ import java.util.List;
 @Slf4j
 public class Initialization implements CommandLineRunner {
     @Autowired
-    private PlatformConfigBean platformConfigBean;
+    private PlatformConfigFile platformConfigFile;
+    @Autowired
+    private ProxyRebateConfigFile proxyRebateConfigFile;
     @Autowired
     private PlatformConfigService platformConfigService;
+    @Autowired
+    private ProxyRebateConfigService proxyRebateConfigService;
     @Override
     public void run(String... args) throws Exception {
         log.info("初始化数据开始============================================》");
+       this.runPlatformConfig();
+       this.runProxyRebateConfig();
+    }
+
+    private void runProxyRebateConfig(){
+        ProxyRebateConfig first = proxyRebateConfigService.findFirst();
+        if (LoginUtil.checkNull(first)){
+            first = new ProxyRebateConfig();
+            first.setFirstMoney(proxyRebateConfigFile.getFirstMoney());
+            first.setFirstProfit(proxyRebateConfigFile.getFirstProfit());
+            first.setSecondMoney(proxyRebateConfigFile.getSecondMoney());
+            first.setSecondProfit(proxyRebateConfigFile.getSecondProfit());
+            first.setThirdMoney(proxyRebateConfigFile.getThirdMoney());
+            first.setThirdProfit(proxyRebateConfigFile.getThirdProfit());
+            first.setFourMoney(proxyRebateConfigFile.getFourMoney());
+            first.setFourProfit(proxyRebateConfigFile.getFourProfit());
+            first.setFiveMoney(proxyRebateConfigFile.getFiveMoney());
+            first.setFiveProfit(proxyRebateConfigFile.getFiveProfit());
+            proxyRebateConfigService.save(first);
+        }
+    }
+    private void runPlatformConfig(){
         List<PlatformConfig> all = platformConfigService.findAll();
         if (LoginUtil.checkNull(all) || all.size()== CommonConst.NUMBER_0){
             PlatformConfig platformConfig = new PlatformConfig();
-            platformConfig.setClearCodeNum(platformConfigBean.getClearCodeNum());
-            platformConfig.setBetRate(platformConfigBean.getBetRate());
-            platformConfig.setChargeMinMoney(platformConfigBean.getChargeMinMoney());
-            platformConfig.setChargeMaxMoney(platformConfigBean.getChargeMaxMoney());
-            platformConfig.setChargeServiceMoney(platformConfigBean.getChargeServiceMoney());
-            platformConfig.setChargeRate(platformConfigBean.getChargeRate());
-            platformConfig.setWithdrawMinMoney(platformConfigBean.getWithdrawMinMoney());
-            platformConfig.setWithdrawMaxMoney(platformConfigBean.getWithdrawMaxMoney());
-            platformConfig.setWithdrawServiceMoney(platformConfigBean.getWithdrawServiceMoney());
-            platformConfig.setWithdrawRate(platformConfigBean.getWithdrawRate());
-            platformConfig.setIpMaxNum(platformConfigBean.getIpMaxNum());
-            platformConfig.setWmMoney(platformConfigBean.getWmMoney());
-            platformConfig.setWmMoneyWarning(platformConfigBean.getWmMoneyWarning());
-            platformConfig.setFirstCommission(platformConfigBean.getFirstCommission());
-            platformConfig.setSecondCommission(platformConfigBean.getSecondCommission());
-            platformConfig.setThirdCommission(platformConfigBean.getThirdCommission());
-            platformConfig.setDomainNameConfiguration(platformConfigBean.getDomainNameConfiguration());
-            platformConfig.setRegisterSwitch(platformConfigBean.getRegisterSwitch());
-            platformConfig.setProxyConfiguration(platformConfigBean.getProxyConfiguration());
+            platformConfig.setClearCodeNum(platformConfigFile.getClearCodeNum());
+            platformConfig.setBetRate(platformConfigFile.getBetRate());
+            platformConfig.setChargeMinMoney(platformConfigFile.getChargeMinMoney());
+            platformConfig.setChargeMaxMoney(platformConfigFile.getChargeMaxMoney());
+            platformConfig.setChargeServiceMoney(platformConfigFile.getChargeServiceMoney());
+            platformConfig.setChargeRate(platformConfigFile.getChargeRate());
+            platformConfig.setWithdrawMinMoney(platformConfigFile.getWithdrawMinMoney());
+            platformConfig.setWithdrawMaxMoney(platformConfigFile.getWithdrawMaxMoney());
+            platformConfig.setWithdrawServiceMoney(platformConfigFile.getWithdrawServiceMoney());
+            platformConfig.setWithdrawRate(platformConfigFile.getWithdrawRate());
+            platformConfig.setIpMaxNum(platformConfigFile.getIpMaxNum());
+            platformConfig.setWmMoney(platformConfigFile.getWmMoney());
+            platformConfig.setWmMoneyWarning(platformConfigFile.getWmMoneyWarning());
+            platformConfig.setFirstCommission(platformConfigFile.getFirstCommission());
+            platformConfig.setSecondCommission(platformConfigFile.getSecondCommission());
+            platformConfig.setThirdCommission(platformConfigFile.getThirdCommission());
+            platformConfig.setDomainNameConfiguration(platformConfigFile.getDomainNameConfiguration());
+            platformConfig.setRegisterSwitch(platformConfigFile.getRegisterSwitch());
+            platformConfig.setProxyConfiguration(platformConfigFile.getProxyConfiguration());
             platformConfigService.save(platformConfig);
         }
     }
