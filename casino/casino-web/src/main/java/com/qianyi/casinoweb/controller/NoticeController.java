5dfcb91b4c331fe5e91fe1fa07d2bfc1526af6ed
benson <liyongqi@0913>
2021-10-16 15:43:57 +0800

    1.公告接口根据前端要求格式化时间，去掉秒

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/NoticeController.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/NoticeController.java
@@ -2,16 +2,20 @@ package com.qianyi.casinoweb.controller;
 
 import com.qianyi.casinocore.model.Notice;
 import com.qianyi.casinocore.service.NoticeService;
+import com.qianyi.casinoweb.vo.NoticeVo;
 import com.qianyi.modulecommon.annotation.NoAuthentication;
 import com.qianyi.modulecommon.reponse.ResponseEntity;
 import com.qianyi.modulecommon.reponse.ResponseUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -25,8 +29,19 @@ public class NoticeController {
     @GetMapping("newest")
     @ApiOperation("公告/活动")
     @NoAuthentication
-    public ResponseEntity<Notice> newest() {
+    public ResponseEntity<NoticeVo> newest() {
         List<Notice> list = noticeService.newest();
-        return ResponseUtil.success(list);
+        List<NoticeVo> voList = new ArrayList<>();
+        if (CollectionUtils.isEmpty(list)) {
+            return ResponseUtil.success(voList);
+        }
+        //前端时间要求去掉秒
+        NoticeVo vo = null;
+        for (Notice notice : list) {
+            vo = new NoticeVo();
+            BeanUtils.copyProperties(notice, vo);
+            voList.add(vo);
+        }
+        return ResponseUtil.success(voList);
     }
 }
