1d39f4ddde5ea6ab1edb1be3122114715b70de1c
kate <kate98765@gmail.com>
2021-11-03 17:08:47 +0800

    提交公司代理报表结果

---
+++ casino/casino-report/src/main/java/com/qianyi/casinoreport/controller/CompanyProxyController.java
@@ -0,0 +1,13 @@
+package com.qianyi.casinoreport.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "报表数据处理")
+@RestController
+@RequestMapping("company")
+public class CompanyProxyController {
+
+
+}
