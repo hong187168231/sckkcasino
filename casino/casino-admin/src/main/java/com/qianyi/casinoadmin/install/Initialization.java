4c83abc3f4cc775bea3462779c21e459c03856de
zhenhao-java <zhenhao.java@gmail.com>
2021-11-04 20:18:52 +0800

    初始话银行数据

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/Initialization.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/Initialization.java
@@ -1,24 +1,31 @@
 package com.qianyi.casinoadmin.install;
 
+import com.qianyi.casinoadmin.install.file.BankInfoImg;
 import com.qianyi.casinoadmin.install.file.PlatformConfigFile;
 import com.qianyi.casinoadmin.install.file.ProxyRebateConfigFile;
 import com.qianyi.casinoadmin.install.file.SysPermissionConfigFile;
 import com.qianyi.casinoadmin.util.LoginUtil;
+import com.qianyi.casinocore.model.BankInfo;
 import com.qianyi.casinocore.model.PlatformConfig;
 import com.qianyi.casinocore.model.RebateConfig;
 import com.qianyi.casinocore.model.SysUser;
+import com.qianyi.casinocore.service.BankInfoService;
 import com.qianyi.casinocore.service.PlatformConfigService;
 import com.qianyi.casinocore.service.RebateConfigService;
 import com.qianyi.casinocore.service.SysUserService;
 import com.qianyi.casinocore.util.CommonConst;
 import com.qianyi.modulecommon.Constants;
 import com.qianyi.modulecommon.reponse.ResponseUtil;
+import com.qianyi.modulecommon.util.CommonUtil;
+import com.qianyi.modulecommon.util.UploadAndDownloadUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -35,14 +42,49 @@ public class Initialization implements CommandLineRunner {
     private SysPermissionConfigFile sysPermissionConfigFile;
     @Autowired
     private SysUserService sysUserService;
+    @Autowired
+    private BankInfoImg bankInfoImg;
+    @Autowired
+    private BankInfoService bankInfoService;
 
     @Override
     public void run(String... args) throws Exception {
-        log.info("初始化数据开始============================================》");
-        this.runAddSysUser();
+       log.info("初始化数据开始============================================》");
+       this.saveBankInfo();
+       this.runAddSysUser();
        this.runPlatformConfig();
        this.runProxyRebateConfig();
        this.runSysPermissionConfig();
+    }
+    private void saveBankInfo(){
+        List<BankInfo> all = bankInfoService.findAll();
+        if (!LoginUtil.checkNull(all) && all.size() > CommonConst.NUMBER_0)
+            return;
+        Map<String, String> bank = BankInfoImg.bank;
+        bank.forEach((key, value) -> {
+            MultipartFile file = bankInfoImg.single(key);
+            this.saveBankInfo(file,value);
+        });
+    }
+    private void saveBankInfo(MultipartFile file,String bankName){
+        BankInfo bankInfo = new BankInfo();
+        bankInfo.setBankName(bankName);
+        bankInfo.setBankType(CommonConst.NUMBER_0);//默认银行卡
+        bankInfo.setDisable(CommonConst.NUMBER_0);//默启用用
+        try {
+            String fileUrl = null;
+            if (file != null){
+                fileUrl = UploadAndDownloadUtil.fileUpload(CommonUtil.getLocalPicPath(), file);
+            }
+            bankInfo.setBankLogo(fileUrl);
+        } catch (Exception e) {
+            bankInfo.setBankLogo("");
+        }
+        try {
+            bankInfoService.saveBankInfo(bankInfo);
+        }catch (Exception e){
+            log.error("重复银行名称");
+        }
     }
 
     private void runAddSysUser() {
