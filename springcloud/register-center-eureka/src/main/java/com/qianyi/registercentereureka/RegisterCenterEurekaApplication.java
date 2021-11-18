42434f6c868ec606bc9f99f33be293ff4009c0b7
dashan <dashan20210805@gmail.com>
2021-11-18 16:17:42 +0800

    打包编译错误问题

--- springcloud/register-center-eureka/src/main/java/com/qianyi/registercentereureka/RegisterCenterEurekaApplication.java
+++ springcloud/register-center-eureka/src/main/java/com/qianyi/registercentereureka/RegisterCenterEurekaApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class})
+@EnableEurekaServer
 public class RegisterCenterEurekaApplication {
 
     public static void main(String[] args) {
