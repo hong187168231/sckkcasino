f515544d53d5c934d6ffbfa2ba20a29d9f083fb5
dashan <dashan20210805@gmail.com>
2021-12-01 18:41:07 +0800

    视迅平台

---
+++ live/live-window/src/main/java/com/qianyi/livewindow/LiveWindowApplication.java
@@ -0,0 +1,13 @@
+package com.qianyi.livewindow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveWindowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveWindowApplication.class, args);
+    }
+
+}
