27b18b71a7fde4185b3354e7eba97ae5dce3d2ee
“tony117269” <tony1172692857@gmail.com>
2021-12-04 22:19:50 +0800

    货币符号logo图片

--- casino/casino-core/src/main/java/com/qianyi/casinocore/model/PlatformConfig.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/PlatformConfig.java
@@ -113,6 +113,13 @@ public class PlatformConfig {
     @ApiModelProperty("读取文件、图片服务器地址")
     private String readUploadUrl;
 
+
+    @ApiModelProperty("log图片地址")
+    private String logImageUrl;
+
+    @ApiModelProperty("金钱符号icon")
+    private String currencyInageUrl;
+
     //得到充值手续费用
     public BigDecimal getChargeServiceCharge(BigDecimal money){
         if (this.chargeRate == null){
