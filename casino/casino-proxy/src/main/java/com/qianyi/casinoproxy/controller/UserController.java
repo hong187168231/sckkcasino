b99da67f555d172bf2a8ca2c0ad4448185117758
zhenhao-java <zhenhao.java@gmail.com>
2021-12-06 22:27:43 +0800

    重置密码日志加上账号

--- casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/UserController.java
+++ casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/UserController.java
@@ -300,7 +300,7 @@ public class UserController {
         if (CasinoProxyUtil.checkNull(user)){
             return ResponseUtil.custom("账户不存在");
         }
-        log.info("重置密码之前密码{}",user.getWithdrawPassword());
+        log.info("重置提现密码之前密码:{}账号:{}",user.getWithdrawPassword(), user.getAccount());
         //随机生成
         String withdrawPassword = PasswordUtil.getRandomPwd();
         String bcryptPassword = CasinoProxyUtil.bcrypt(withdrawPassword);
@@ -324,7 +324,7 @@ public class UserController {
         if (CasinoProxyUtil.checkNull(user)){
             return ResponseUtil.custom("账户不存在");
         }
-        log.info("重置密码之前密码{}",user.getPassword());
+        log.info("重置密码之前密码:{}账号:{}",user.getPassword(), user.getAccount());
         //随机生成
         String password = PasswordUtil.getRandomPwd();
         String bcryptPassword = CasinoProxyUtil.bcrypt(password);
