5eb6d854628f3927baab908faf311053067d1030
benson <bb0491742@gmail.com>
2021-12-13 19:56:23 +0800

    客服接口新增在线沟通地址和手机号

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/CustomerController.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.qianyi.casinoweb.controller;
 
 import com.qianyi.casinocore.model.CustomerConfigure;
 import com.qianyi.casinocore.service.CustomerConfigureService;
+import com.qianyi.casinoweb.vo.CustomerVo;
 import com.qianyi.modulecommon.Constants;
 import com.qianyi.modulecommon.annotation.NoAuthentication;
 import com.qianyi.modulecommon.reponse.ResponseEntity;
@@ -9,10 +10,12 @@ import com.qianyi.modulecommon.reponse.ResponseUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -26,8 +29,22 @@ public class CustomerController {
     @GetMapping("contact")
     @ApiOperation("客服联系方式")
     @NoAuthentication
-    public ResponseEntity<List<CustomerConfigure>> contact() {
+    public ResponseEntity<CustomerVo> contact() {
         List<CustomerConfigure> list = customerConfigureService.findByState(Constants.open);
-        return ResponseUtil.success(list);
+        CustomerVo vo = new CustomerVo();
+        Iterator<CustomerConfigure> integerIterator = list.iterator();
+        while(integerIterator.hasNext()) {
+            CustomerConfigure customerConfigure = integerIterator.next();
+            if (customerConfigure.getCustomerMark() == 5) {
+                vo.setTelephone(customerConfigure);
+                integerIterator.remove();
+            }
+            if (customerConfigure.getCustomerMark() == 6) {
+                vo.setOnlineUrl(customerConfigure);
+                integerIterator.remove();
+            }
+        }
+        vo.setCustomerList(list);
+        return ResponseUtil.success(vo);
     }
 }
