35d705a5f2e5cd57b22122af4577f6d12494907d
benson <bb0491742@gmail.com>
2021-12-13 19:56:23 +0800

    客服接口新增在线沟通地址和手机号

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/CustomerController.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/CustomerController.java
@@ -1,19 +1,21 @@
 package com.qianyi.casinoweb.controller;
 
-import com.qianyi.casinocore.model.Customer;
-import com.qianyi.casinocore.service.CustomerService;
+import com.qianyi.casinocore.model.CustomerConfigure;
+import com.qianyi.casinocore.service.CustomerConfigureService;
+import com.qianyi.casinoweb.vo.CustomerVo;
+import com.qianyi.modulecommon.Constants;
 import com.qianyi.modulecommon.annotation.NoAuthentication;
 import com.qianyi.modulecommon.reponse.ResponseEntity;
 import com.qianyi.modulecommon.reponse.ResponseUtil;
-import com.qianyi.modulecommon.util.CommonUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.ObjectUtils;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -22,16 +24,27 @@ import java.util.List;
 public class CustomerController {
 
     @Autowired
-    CustomerService customerService;
+    private CustomerConfigureService customerConfigureService;
 
     @GetMapping("contact")
     @ApiOperation("客服联系方式")
     @NoAuthentication
-    public ResponseEntity<Customer> contact() {
-        Customer customer = customerService.findFirst();
-        if (customer != null) {
-            return ResponseUtil.success(customer);
+    public ResponseEntity<CustomerVo> contact() {
+        List<CustomerConfigure> list = customerConfigureService.findByState(Constants.open);
+        CustomerVo vo = new CustomerVo();
+        Iterator<CustomerConfigure> integerIterator = list.iterator();
+        while(integerIterator.hasNext()) {
+            CustomerConfigure customerConfigure = integerIterator.next();
+            if (customerConfigure.getCustomerMark() == 5) {
+                vo.setTelephone(customerConfigure);
+                integerIterator.remove();
+            }
+            if (customerConfigure.getCustomerMark() == 6) {
+                vo.setOnlineUrl(customerConfigure);
+                integerIterator.remove();
+            }
         }
-        return ResponseUtil.success(new Customer());
+        vo.setCustomerList(list);
+        return ResponseUtil.success(vo);
     }
 }
