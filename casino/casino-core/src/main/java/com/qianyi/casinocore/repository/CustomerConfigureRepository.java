31fc8c95a13c2dd4656b23426595f104fc386c1d
Lulu <luli13695@gmail.com>
2021-12-13 17:06:00 +0700

    客服中心配置service

---
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/CustomerConfigureRepository.java
@@ -0,0 +1,7 @@
+package com.qianyi.casinocore.repository;
+
+import com.qianyi.casinocore.model.CustomerConfigure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerConfigureRepository extends JpaRepository<CustomerConfigure,Long> {
+}
