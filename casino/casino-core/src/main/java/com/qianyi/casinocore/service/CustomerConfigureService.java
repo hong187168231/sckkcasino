b0eefc8f1b98d98ce9f395248afb20e356f1b028
benson <bb0491742@gmail.com>
2021-12-13 18:41:47 +0800

    删除无用代码

---
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/service/CustomerConfigureService.java
@@ -0,0 +1,36 @@
+package com.qianyi.casinocore.service;
+
+import com.qianyi.casinocore.model.CustomerConfigure;
+import com.qianyi.casinocore.repository.CustomerConfigureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerConfigureService {
+    @Autowired
+    CustomerConfigureRepository customerConfigureRepository;
+
+    public List<CustomerConfigure> findAll() {
+        return customerConfigureRepository.findAll();
+    }
+
+    @CacheEvict(cacheNames = "customerConfigure", allEntries = true)
+    public CustomerConfigure save(CustomerConfigure customerConfigure){
+        return customerConfigureRepository.save(customerConfigure);
+    }
+
+    public CustomerConfigure getById(Long id){
+        return customerConfigureRepository.getById(id);
+    }
+
+    @Cacheable(cacheNames = "customerConfigure")
+    public List<CustomerConfigure> findByState(Integer state){
+        return customerConfigureRepository.findByState(state);
+    }
+}
