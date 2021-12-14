0312863e580a35116ecbebcc43c084f33dcbc8d1
benson <bb0491742@gmail.com>
2021-12-14 14:02:44 +0800

    获取客服脚本编码接口

---
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/PlatformConfigController.java
@@ -0,0 +1,38 @@
+package com.qianyi.casinoweb.controller;
+
+import com.qianyi.casinocore.model.PlatformConfig;
+import com.qianyi.casinocore.service.PlatformConfigService;
+import com.qianyi.modulecommon.annotation.NoAuthentication;
+import com.qianyi.modulecommon.reponse.ResponseEntity;
+import com.qianyi.modulecommon.reponse.ResponseUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("platformConfig")
+@Api(tags = "平台配置中心")
+@Slf4j
+public class PlatformConfigController {
+
+    @Autowired
+    private PlatformConfigService platformConfigService;
+
+
+    @GetMapping("getCustomerCode")
+    @ApiOperation("获取客服脚本编码")
+    @NoAuthentication
+    public ResponseEntity<String> getCustomerCode() {
+        PlatformConfig platformConfig = platformConfigService.findFirst();
+        if (platformConfig == null) {
+            return ResponseUtil.success();
+        }
+        String customerCode = platformConfig.getCustomerCode();
+        return ResponseUtil.success(customerCode);
+    }
+
+}
