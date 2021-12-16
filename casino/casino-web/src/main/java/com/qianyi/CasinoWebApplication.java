67a5bd465bdc708e1fe9fb69593a0e7b2b7e2495
benson <bb0491742@gmail.com>
2021-12-16 15:22:25 +0800

    master部署测试加日志打印

--- casino/casino-web/src/main/java/com/qianyi/CasinoWebApplication.java
+++ casino/casino-web/src/main/java/com/qianyi/CasinoWebApplication.java
@@ -1,5 +1,6 @@
 package com.qianyi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,9 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@Slf4j
 public class CasinoWebApplication {
 
     public static void main(String[] args) {
+        log.info("master环境部署成功");
         SpringApplication.run(CasinoWebApplication.class, args);
     }
 
