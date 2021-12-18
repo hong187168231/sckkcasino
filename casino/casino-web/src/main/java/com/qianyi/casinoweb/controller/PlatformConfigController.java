25cc4d9e0e835fed2c7212424cdedede5dec67eb
benson <bb0491742@gmail.com>
2021-12-18 20:10:08 +0800

    新增获取人人代三级分佣比例接口

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/PlatformConfigController.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/controller/PlatformConfigController.java
@@ -6,12 +6,18 @@ import com.qianyi.modulecommon.annotation.NoAuthentication;
 import com.qianyi.modulecommon.reponse.ResponseEntity;
 import com.qianyi.modulecommon.reponse.ResponseUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("platformConfig")
@@ -21,7 +27,6 @@ public class PlatformConfigController {
 
     @Autowired
     private PlatformConfigService platformConfigService;
-
 
     @GetMapping("getCustomerCode")
     @ApiOperation("获取客服脚本编码")
@@ -35,4 +40,39 @@ public class PlatformConfigController {
         return ResponseUtil.success(customerCode);
     }
 
+    @GetMapping("checkPeopleProxySwitch")
+    @ApiOperation("检查人人代开关")
+    @NoAuthentication
+    public ResponseEntity<Boolean> checkPeopleProxySwitch() {
+        PlatformConfig platformConfig = platformConfigService.findFirst();
+        boolean proxySwitch = PlatformConfig.checkPeopleProxySwitch(platformConfig);
+        return ResponseUtil.success(proxySwitch);
+    }
+
+    @GetMapping("getPeopleProportion")
+    @ApiOperation("获取人人代三级分佣比例")
+    @NoAuthentication
+    public ResponseEntity<PeopleProportion> getPeopleProportion() {
+        PlatformConfig platformConfig = platformConfigService.findFirst();
+        if (platformConfig == null) {
+            return ResponseUtil.success();
+        }
+        PeopleProportion peopleProportion = new PeopleProportion();
+        BeanUtils.copyProperties(platformConfig, peopleProportion);
+        return ResponseUtil.success(peopleProportion);
+    }
+
+    @Data
+    @ApiModel("人人代三级分佣比例")
+    class PeopleProportion{
+
+        @ApiModelProperty("一级玩家返佣")
+        private BigDecimal firstCommission;
+
+        @ApiModelProperty("二级玩家返佣")
+        private BigDecimal secondCommission;
+
+        @ApiModelProperty("三级玩家返佣")
+        private BigDecimal thirdCommission;
+    }
 }
