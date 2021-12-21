d5385c3fae669cf9753806e593d56c0556503523
kate <kate98765@gmail.com>
2021-12-21 22:28:10 +0800

    个人报表,计算方式更改

--- casino/casino-admin/src/test/java/com/qianyi/casinoadmin/controller/ReportControllerTest.java
+++ casino/casino-admin/src/test/java/com/qianyi/casinoadmin/controller/ReportControllerTest.java
@@ -1,5 +1,6 @@
 package com.qianyi.casinoadmin.controller;
 
+import com.qianyi.casinoadmin.vo.HistoryTotal;
 import com.qianyi.casinocore.repository.CompanyProxyMonthRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Map;
 
@@ -49,6 +52,18 @@ public class ReportControllerTest {
         result.keySet().forEach(item ->{
             log.info("{}:{}",item,result.get(item).toString());
         });
-        log.info("{}",result);
+
+        HistoryTotal historyTotal = new HistoryTotal();
+        historyTotal.setAll_profit_amount(new BigDecimal(result.get("all_profit_amount").toString()).setScale(2, RoundingMode.HALF_UP));
+        historyTotal.setService_charge(new BigDecimal(result.get("service_charge").toString()).setScale(2, RoundingMode.HALF_UP));
+        historyTotal.setTotal_amount(new BigDecimal(result.get("total_amount").toString()).setScale(2, RoundingMode.HALF_UP));
+        historyTotal.setNum(Integer.parseInt(result.get("num").toString()));
+        historyTotal.setBet_amount(new BigDecimal(result.get("bet_amount").toString()).setScale(2, RoundingMode.HALF_UP));
+        historyTotal.setWin_loss(new BigDecimal(result.get("win_loss").toString()).setScale(2, RoundingMode.HALF_UP));
+        historyTotal.setValidbet(new BigDecimal(result.get("validbet").toString()).setScale(2, RoundingMode.HALF_UP));
+        historyTotal.setWash_amount(new BigDecimal(result.get("wash_amount").toString()).setScale(2, RoundingMode.HALF_UP));
+        historyTotal.setAvg_benefit(new BigDecimal(result.get("avg_benefit").toString()).setScale(2, RoundingMode.HALF_UP));
+
+        log.info("{}",historyTotal);
     }
 }
