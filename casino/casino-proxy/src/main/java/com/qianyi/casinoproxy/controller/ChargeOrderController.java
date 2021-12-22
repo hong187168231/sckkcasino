b7ce6108ee12bd403703f0228afaf516ba9602a5
zhenhao-java <zhenhao.java@gmail.com>
2021-12-22 16:22:18 +0800

    注释上分下分审核注单

--- casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/ChargeOrderController.java
+++ casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/ChargeOrderController.java
@@ -1,187 +1,171 @@
-package com.qianyi.casinoproxy.controller;
-
-import com.qianyi.casinocore.business.ChargeOrderBusiness;
-import com.qianyi.casinocore.model.*;
-import com.qianyi.casinocore.service.*;
-import com.qianyi.casinocore.util.CommonConst;
-import com.qianyi.casinocore.vo.ChargeOrderVo;
-import com.qianyi.casinocore.vo.PageResultVO;
-import com.qianyi.casinoproxy.util.CasinoProxyUtil;
-import com.qianyi.modulecommon.annotation.NoAuthorization;
-import com.qianyi.modulecommon.reponse.ResponseEntity;
-import com.qianyi.modulecommon.reponse.ResponseUtil;
-import com.qianyi.modulecommon.util.CommonUtil;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-/**
- * 充值申请
- */
-@Slf4j
-@RestController
-@RequestMapping("/chargeOrder")
-@Api(tags = "资金中心")
-public class ChargeOrderController {
-    @Autowired
-    private ChargeOrderService chargeOrderService;
-
-    @Autowired
-    private ChargeOrderBusiness chargeOrderBusiness;
-
-    @Autowired
-    private CollectionBankcardService collectionBankcardService;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private ProxyUserService proxyUserService;
-    /**
-     * 充值申请列表
-     *
-     * @param status 状态(0未确认 1已确认)
-     * @param orderNo 充值订单号
-     * @param account 会员账号 account
-     * @return
-     */
-    @ApiOperation("充值申请列表")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "pageSize", value = "每页大小(默认10条)", required = false),
-            @ApiImplicitParam(name = "pageCode", value = "当前页(默认第一页)", required = false),
-            @ApiImplicitParam(name = "status", value = "状态(0未确认 1已确认)", required = false),
-            @ApiImplicitParam(name = "orderNo", value = "订单号", required = false),
-            @ApiImplicitParam(name = "account", value = "会员账号", required = false),
-    })
-    @GetMapping("/chargeOrderList")
-    public ResponseEntity<ChargeOrderVo> chargeOrderList(Integer pageSize, Integer pageCode, Integer status, String orderNo, String account){
-        Sort sort = Sort.by("id").descending();
-        Pageable pageable = CasinoProxyUtil.setPageable(pageCode, pageSize, sort);
-        ChargeOrder order = new ChargeOrder();
-        if (CasinoProxyUtil.setParameter(order)){
-            return ResponseUtil.custom(CommonConst.NETWORK_ANOMALY);
-        }
-        order.setStatus(status);
-        order.setOrderNo(orderNo);
-        if (!CasinoProxyUtil.checkNull(account)){
-            User user = userService.findByAccount(account);
-            if (CasinoProxyUtil.checkNull(user)){
-                return ResponseUtil.custom("用户不存在");
-            }
-            order.setUserId(user.getId());
-        }
-        Page<ChargeOrder> chargeOrderPage = chargeOrderService.findChargeOrderPage(order, pageable);
-        PageResultVO<ChargeOrderVo> pageResultVO =new PageResultVO(chargeOrderPage);
-        List<ChargeOrder> content = chargeOrderPage.getContent();
-        if(content != null && content.size() > 0){
-            List<ChargeOrderVo> chargeOrderVoList = new LinkedList<>();
-            List<Long> userIds = content.stream().map(ChargeOrder::getUserId).collect(Collectors.toList());
-            List<Long> collect = content.stream().map(ChargeOrder::getBankcardId).collect(Collectors.toList());
-            List<User> userList = userService.findAll(userIds);
-            List<CollectionBankcard> all = collectionBankcardService.findAll(collect);
-//            List<String> updateBys = content.stream().map(ChargeOrder::getUpdateBy).collect(Collectors.toList());
-//            List<ProxyUser> proxyUsers = proxyUserService.findProxyUsers(updateBys);
-            Map<Long, CollectionBankcard> bankcardMap = all.stream().collect(Collectors.toMap(CollectionBankcard::getId, a -> a, (k1, k2) -> k1));
-            if(userList != null){
-                content.stream().forEach(chargeOrder ->{
-                    ChargeOrderVo chargeOrderVo = new ChargeOrderVo(chargeOrder);
-                    userList.stream().forEach(user->{
-                        if (user.getId().equals(chargeOrder.getUserId())){
-                            chargeOrderVo.setAccount(user.getAccount());
-                            this.setCollectionBankcard(bankcardMap.get(chargeOrder.getBankcardId()),chargeOrderVo);
-                        }
-                    });
-//                    proxyUsers.stream().forEach(proxyUser->{
-//                        if (chargeOrder.getStatus() != CommonConst.NUMBER_0 && proxyUser.getId().toString().equals(chargeOrder.getUpdateBy() == null?"":chargeOrder.getUpdateBy())){
-//                            chargeOrderVo.setUpdateBy(proxyUser.getUserName());
+//package com.qianyi.casinoproxy.controller;
+//
+//import com.qianyi.casinocore.business.ChargeOrderBusiness;
+//import com.qianyi.casinocore.model.*;
+//import com.qianyi.casinocore.service.*;
+//import com.qianyi.casinocore.util.CommonConst;
+//import com.qianyi.casinocore.vo.ChargeOrderVo;
+//import com.qianyi.casinocore.vo.PageResultVO;
+//import com.qianyi.casinoproxy.util.CasinoProxyUtil;
+//import com.qianyi.modulecommon.reponse.ResponseEntity;
+//import com.qianyi.modulecommon.reponse.ResponseUtil;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiImplicitParam;
+//import io.swagger.annotations.ApiImplicitParams;
+//import io.swagger.annotations.ApiOperation;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.domain.Sort;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.LinkedList;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//
+///**
+// * 充值申请
+// */
+//@Slf4j
+//@RestController
+//@RequestMapping("/chargeOrder")
+//@Api(tags = "资金中心")
+//public class ChargeOrderController {
+//    @Autowired
+//    private ChargeOrderService chargeOrderService;
+//
+//    @Autowired
+//    private ChargeOrderBusiness chargeOrderBusiness;
+//
+//    @Autowired
+//    private CollectionBankcardService collectionBankcardService;
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @Autowired
+//    private ProxyUserService proxyUserService;
+//    /**
+//     * 充值申请列表
+//     *
+//     * @param status 状态(0未确认 1已确认)
+//     * @param orderNo 充值订单号
+//     * @param account 会员账号 account
+//     * @return
+//     */
+//    @ApiOperation("充值申请列表")
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "pageSize", value = "每页大小(默认10条)", required = false),
+//            @ApiImplicitParam(name = "pageCode", value = "当前页(默认第一页)", required = false),
+//            @ApiImplicitParam(name = "status", value = "状态(0未确认 1已确认)", required = false),
+//            @ApiImplicitParam(name = "orderNo", value = "订单号", required = false),
+//            @ApiImplicitParam(name = "account", value = "会员账号", required = false),
+//    })
+//    @GetMapping("/chargeOrderList")
+//    public ResponseEntity<ChargeOrderVo> chargeOrderList(Integer pageSize, Integer pageCode, Integer status, String orderNo, String account){
+//        Sort sort = Sort.by("id").descending();
+//        Pageable pageable = CasinoProxyUtil.setPageable(pageCode, pageSize, sort);
+//        ChargeOrder order = new ChargeOrder();
+//        if (CasinoProxyUtil.setParameter(order)){
+//            return ResponseUtil.custom(CommonConst.NETWORK_ANOMALY);
+//        }
+//        order.setStatus(status);
+//        order.setOrderNo(orderNo);
+//        if (!CasinoProxyUtil.checkNull(account)){
+//            User user = userService.findByAccount(account);
+//            if (CasinoProxyUtil.checkNull(user)){
+//                return ResponseUtil.custom("用户不存在");
+//            }
+//            order.setUserId(user.getId());
+//        }
+//        Page<ChargeOrder> chargeOrderPage = chargeOrderService.findChargeOrderPage(order, pageable);
+//        PageResultVO<ChargeOrderVo> pageResultVO =new PageResultVO(chargeOrderPage);
+//        List<ChargeOrder> content = chargeOrderPage.getContent();
+//        if(content != null && content.size() > 0){
+//            List<ChargeOrderVo> chargeOrderVoList = new LinkedList<>();
+//            List<Long> userIds = content.stream().map(ChargeOrder::getUserId).collect(Collectors.toList());
+//            List<Long> collect = content.stream().map(ChargeOrder::getBankcardId).collect(Collectors.toList());
+//            List<User> userList = userService.findAll(userIds);
+//            List<CollectionBankcard> all = collectionBankcardService.findAll(collect);
+//            Map<Long, CollectionBankcard> bankcardMap = all.stream().collect(Collectors.toMap(CollectionBankcard::getId, a -> a, (k1, k2) -> k1));
+//            if(userList != null){
+//                content.stream().forEach(chargeOrder ->{
+//                    ChargeOrderVo chargeOrderVo = new ChargeOrderVo(chargeOrder);
+//                    userList.stream().forEach(user->{
+//                        if (user.getId().equals(chargeOrder.getUserId())){
+//                            chargeOrderVo.setAccount(user.getAccount());
+//                            this.setCollectionBankcard(bankcardMap.get(chargeOrder.getBankcardId()),chargeOrderVo);
 //                        }
 //                    });
-                    chargeOrderVoList.add(chargeOrderVo);
-                });
-            }
-            pageResultVO.setContent(chargeOrderVoList);
-        }
-        return ResponseUtil.success(pageResultVO);
-    }
-    private void  setCollectionBankcard(CollectionBankcard collectionBankcard,ChargeOrderVo chargeOrderVo){
-        if (CasinoProxyUtil.checkNull(collectionBankcard)){
-            return ;
-        }
-        chargeOrderVo.setBankNo(collectionBankcard.getBankNo());
-        chargeOrderVo.setAccountName(collectionBankcard.getAccountName());
-    }
-
-    /**
-     * 充值申请列表
-     *
-     * @param id 充值订单id
-     * @param status 汇款状态，0.未确认。 1.成功   2.失败, 3.失效
-     * @return
-     */
-    @ApiOperation("审核充值订单")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "订单id", required = true),
-            @ApiImplicitParam(name = "status", value = "汇款状态， 1.成功  2.失败", required = true),
-            @ApiImplicitParam(name = "remark", value = "备注", required = false),
-//            @ApiImplicitParam(name = "money", value = "上分金额", required = false),
-    })
-    @PostMapping("/updateChargeOrder")
-    public ResponseEntity updateChargeOrder(Long id, Integer status,String remark){
-        if (CasinoProxyUtil.checkNull(id,status)){
-            ResponseUtil.custom("参数不合法");
-        }
-        if(status != CommonConst.NUMBER_1 && status != CommonConst.NUMBER_2){
-            return ResponseUtil.custom("参数不合法");
-        }
-        Long authId = CasinoProxyUtil.getAuthId();
-        ProxyUser byId = proxyUserService.findById(authId);
-        String lastModifier = (byId == null || byId.getUserName() == null)? "" : byId.getUserName();
-        return chargeOrderBusiness.checkOrderSuccess(id,status,remark,lastModifier);
-    }
-
-
-
-    /**
-     * 修改充值备注
-     *
-     * @return
-     */
-    @ApiOperation("修改充值备注")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "订单id", required = true),
-            @ApiImplicitParam(name = "remark", value = "备注", required = false)
-    })
-    @PostMapping("/updateChargeOrdersRemark")
-    public ResponseEntity updateChargeOrdersRemark(Long id,String remark){
-        if (CasinoProxyUtil.checkNull(id)){
-            ResponseUtil.custom("参数不合法");
-        }
-        ChargeOrder order = chargeOrderService.findChargeOrderByIdUseLock(id);
-        if(order == null){
-            return ResponseUtil.custom("订单不存在或已被处理");
-        }
-        chargeOrderService.updateChargeOrdersRemark(remark,order.getId());
-        return ResponseUtil.success();
-    }
-}
+//                    chargeOrderVoList.add(chargeOrderVo);
+//                });
+//            }
+//            pageResultVO.setContent(chargeOrderVoList);
+//        }
+//        return ResponseUtil.success(pageResultVO);
+//    }
+//    private void  setCollectionBankcard(CollectionBankcard collectionBankcard,ChargeOrderVo chargeOrderVo){
+//        if (CasinoProxyUtil.checkNull(collectionBankcard)){
+//            return ;
+//        }
+//        chargeOrderVo.setBankNo(collectionBankcard.getBankNo());
+//        chargeOrderVo.setAccountName(collectionBankcard.getAccountName());
+//    }
+//
+//    /**
+//     * 充值申请列表
+//     *
+//     * @param id 充值订单id
+//     * @param status 汇款状态，0.未确认。 1.成功   2.失败, 3.失效
+//     * @return
+//     */
+//    @ApiOperation("审核充值订单")
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "id", value = "订单id", required = true),
+//            @ApiImplicitParam(name = "status", value = "汇款状态， 1.成功  2.失败", required = true),
+//            @ApiImplicitParam(name = "remark", value = "备注", required = false),
+//    })
+//    @PostMapping("/updateChargeOrder")
+//    public ResponseEntity updateChargeOrder(Long id, Integer status,String remark){
+//        if (CasinoProxyUtil.checkNull(id,status)){
+//            ResponseUtil.custom("参数不合法");
+//        }
+//        if(status != CommonConst.NUMBER_1 && status != CommonConst.NUMBER_2){
+//            return ResponseUtil.custom("参数不合法");
+//        }
+//        Long authId = CasinoProxyUtil.getAuthId();
+//        ProxyUser byId = proxyUserService.findById(authId);
+//        String lastModifier = (byId == null || byId.getUserName() == null)? "" : byId.getUserName();
+//        return chargeOrderBusiness.checkOrderSuccess(id,status,remark,lastModifier);
+//    }
+//
+//
+//
+//    /**
+//     * 修改充值备注
+//     *
+//     * @return
+//     */
+//    @ApiOperation("修改充值备注")
+//    @ApiImplicitParams({
+//            @ApiImplicitParam(name = "id", value = "订单id", required = true),
+//            @ApiImplicitParam(name = "remark", value = "备注", required = false)
+//    })
+//    @PostMapping("/updateChargeOrdersRemark")
+//    public ResponseEntity updateChargeOrdersRemark(Long id,String remark){
+//        if (CasinoProxyUtil.checkNull(id)){
+//            ResponseUtil.custom("参数不合法");
+//        }
+//        ChargeOrder order = chargeOrderService.findChargeOrderByIdUseLock(id);
+//        if(order == null){
+//            return ResponseUtil.custom("订单不存在或已被处理");
+//        }
+//        chargeOrderService.updateChargeOrdersRemark(remark,order.getId());
+//        return ResponseUtil.success();
+//    }
+//}
