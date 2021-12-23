9fe6be86380241e6c64a074553dbce34f9eb8875
“tony117269” <tony1172692857@gmail.com>
2021-12-23 21:55:33 +0800

    刷新token机制改到后端

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/inteceptor/AuthenticationInteceptor.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/inteceptor/AuthenticationInteceptor.java
@@ -7,8 +7,10 @@ import com.qianyi.casinocore.service.SysUserService;
 import com.qianyi.casinocore.service.UserService;
 import com.qianyi.modulecommon.Constants;
 import com.qianyi.modulecommon.inteceptor.AbstractAuthenticationInteceptor;
+import com.qianyi.modulecommon.util.CommonUtil;
 import com.qianyi.modulejjwt.JjwtUtil;
 import com.qianyi.modulespringcacheredis.util.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -16,6 +18,7 @@ import org.springframework.util.ObjectUtils;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @Component
 public class AuthenticationInteceptor extends AbstractAuthenticationInteceptor {
 
@@ -44,7 +47,61 @@ public class AuthenticationInteceptor extends AbstractAuthenticationInteceptor {
         if(JjwtUtil.check(token, "casino-admin")){
             return true;
         }
-        return false;
+        //token过期获取新token
+        JjwtUtil.Token refreshJwtToken = refreshJwtToken(token);
+        if (refreshJwtToken == null) {
+            return false;
+        }
+        String newToken = refreshJwtToken.getNewToken();
+        response.setHeader(Constants.AUTHORIZATION, newToken);
+        return true;
+    }
+
+    /**
+     * 通过旧token获取新token
+     * @param token
+     * @return
+     */
+    public JjwtUtil.Token refreshJwtToken(String token) {
+        boolean checkNull = CommonUtil.checkNull(token);
+        if (checkNull) {
+            return null;
+        }
+        JjwtUtil.Subject subject = JjwtUtil.getSubject(token);
+        if (subject == null || ObjectUtils.isEmpty(subject.getUserId())) {
+            return null;
+        }
+        //获取登陆用户
+        Long authId = Long.parseLong(subject.getUserId());
+        SysUser user = sysUserService.findById(authId);
+        synchronized (token.intern()) {
+            //多个请求只有一个去刷新token
+            Object redisToken = redisUtil.get(Constants.REDIS_TOKEN_ADMIN + authId);
+            JjwtUtil.Token redisJwtToken = null;
+            if (redisToken != null) {
+                redisJwtToken = (JjwtUtil.Token) redisToken;
+            }
+            //判断其他请求是否已经获取到新token
+            if (redisJwtToken != null && token.equals(redisJwtToken.getOldToken()) && !ObjectUtils.isEmpty(redisJwtToken.getNewToken())) {
+                return redisJwtToken;
+            }
+            //获取新token
+            String refreshToken = JjwtUtil.refreshToken(token, user.getPassWord(), Constants.ADMIN_REFRESH_TTL, Constants.CASINO_ADMIN);
+            if (ObjectUtils.isEmpty(refreshToken)) {
+                return null;
+            }
+            //获取到新token后会把之前的token设置成旧的，用于判断后面其他带旧token的请求，有一个旧token获取到新token，其他直接从redis取新的
+            JjwtUtil.Token jwtTiken = new JjwtUtil.Token();
+            jwtTiken.setOldToken(token);
+            jwtTiken.setNewToken(refreshToken);
+            //不是最新的token也可以获取到新token，但是多设备校验的时候会拦截
+            if (redisJwtToken != null && (token.equals(redisJwtToken.getOldToken()) || token.equals(redisJwtToken.getNewToken()))) {
+                redisUtil.set(Constants.REDIS_TOKEN_ADMIN + authId, jwtTiken, Constants.ADMIN_REFRESH_TTL);
+            } else {
+                log.error("当前token={}，iss={} 已失效刷新token无效，redis中token信息为={}", token, Constants.CASINO_WEB, redisJwtToken);
+            }
+            return jwtTiken;
+        }
     }
 
     @Override
