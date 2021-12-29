67ef05b541bcd32139a5940d4a33cbcde9d577cd
zhenhao-java <zhenhao.java@gmail.com>
2021-12-29 14:06:55 +0800

    增加proxy日志输出

--- casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/config/security/filter/MyAuthenticationFilter.java
+++ casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/config/security/filter/MyAuthenticationFilter.java
@@ -10,6 +10,7 @@ import com.qianyi.casinoproxy.config.security.util.MultiReadHttpServletResponse;
 import com.qianyi.casinoproxy.util.CasinoProxyUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -25,6 +26,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -88,6 +90,8 @@ public class MyAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String logRequestBody(MultiReadHttpServletRequest request) {
+        //日志打印traceId，同一次请求的traceId相同，方便定位日志
+        ThreadContext.put("traceId", UUID.randomUUID().toString().replaceAll("-",""));
         MultiReadHttpServletRequest wrapper = request;
         if (wrapper != null) {
             try {
