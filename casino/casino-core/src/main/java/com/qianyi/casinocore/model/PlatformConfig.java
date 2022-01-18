557ea1b189486e4b7d4d4b85bd5ccf51978d5db3
zhenhao-java <zhenhao.java@gmail.com>
2022-01-18 15:44:24 +0800

    修改表结构

--- casino/casino-core/src/main/java/com/qianyi/casinocore/model/PlatformConfig.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/PlatformConfig.java
@@ -22,7 +22,7 @@ public class PlatformConfig {
     private Long id;
 
     @ApiModelProperty("最低金额清除打码量")
-    @Column(columnDefinition = "Decimal(10,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal clearCodeNum;
 
     @ApiModelProperty("打码倍率")
@@ -30,15 +30,15 @@ public class PlatformConfig {
     private BigDecimal betRate;
 
     @ApiModelProperty("每笔最低充值")
-    @Column(columnDefinition = "Decimal(15,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal chargeMinMoney;
 
     @ApiModelProperty("每笔最高充值")
-    @Column(columnDefinition = "Decimal(15,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal chargeMaxMoney;
 
     @ApiModelProperty("充值服务费")
-    @Column(columnDefinition = "Decimal(15,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal chargeServiceMoney;
 
     @ApiModelProperty("充值手续费百分比")
@@ -46,15 +46,15 @@ public class PlatformConfig {
     private BigDecimal chargeRate;
 
     @ApiModelProperty("每笔最低提现额")
-    @Column(columnDefinition = "Decimal(15,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal withdrawMinMoney;
 
     @ApiModelProperty("每笔最高提现额")
-    @Column(columnDefinition = "Decimal(15,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal withdrawMaxMoney;
 
     @ApiModelProperty("提现服务费")
-    @Column(columnDefinition = "Decimal(10,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal withdrawServiceMoney;
 
     @ApiModelProperty("提现手续费百分比")
@@ -65,11 +65,11 @@ public class PlatformConfig {
     private Integer ipMaxNum;
 
     @ApiModelProperty("WM余额")
-    @Column(columnDefinition = "Decimal(15,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal wmMoney;
 
     @ApiModelProperty("WM余额警戒线")
-    @Column(columnDefinition = "Decimal(15,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal wmMoneyWarning;
 
     @ApiModelProperty("一级玩家返佣")
@@ -101,7 +101,7 @@ public class PlatformConfig {
     private Integer registerSwitch = 1;
 
     @ApiModelProperty("短信余额警戒线")
-    @Column(columnDefinition = "Decimal(10,6) default '0.00'")
+    @Column(columnDefinition = "Decimal(19,6) default '0.00'")
     private BigDecimal sendMessageWarning;
 
     @ApiModelProperty("人人代直属下级最大个数")
