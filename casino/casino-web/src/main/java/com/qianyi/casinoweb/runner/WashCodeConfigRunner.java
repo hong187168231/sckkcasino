4ad7dc308f4934ad99b85549905962d1d35dc15b
benson <bb0491742@gmail.com>
2022-02-10 18:45:50 +0800

    洗码初始化新增PG、CQ9

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/runner/WashCodeConfigRunner.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/runner/WashCodeConfigRunner.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -28,34 +29,49 @@ public class WashCodeConfigRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         log.info("开始初始化洗码配置");
         List<WashCodeConfig> list = washCodeConfigService.findByPlatform(Constants.PLATFORM);
-        log.info("洗码配置查询结果data={}",list);
-        if (!CollectionUtils.isEmpty(list)) {
-            log.info("洗码配置已初始化，无需再次初始化");
+        log.info("洗码配置查询结果data={}", list);
+        if (CollectionUtils.isEmpty(list)) {
+            List<WashCodeConfig> washCodeConfigs = initData();
+            washCodeConfigService.saveAll(washCodeConfigs);
             return;
         }
-        initData();
+        List<WashCodeConfig> washCodeConfigs = initData();
+        for (WashCodeConfig codeConfig : list) {
+            Iterator<WashCodeConfig> configIterator = washCodeConfigs.iterator();
+            while (configIterator.hasNext()) {
+                WashCodeConfig wash = configIterator.next();
+                if (wash.getGameId().equals(codeConfig.getGameId())) {
+                    configIterator.remove();
+                }
+            }
+        }
+        if (!CollectionUtils.isEmpty(washCodeConfigs)) {
+            washCodeConfigService.saveAll(washCodeConfigs);
+        }
         log.info("洗码配置初始化完成");
     }
 
-    private void initData() {
+    private List<WashCodeConfig> initData() {
         List<WashCodeConfig> list = new ArrayList<>();
-        list.add(setData("101", "百家乐","Baccarat"));
-        list.add(setData("102", "龙虎","Dragon Tiger"));
-        list.add(setData("103", "轮盘","Roulette"));
-        list.add(setData("104", "骰宝","SicBo"));
-        list.add(setData("105", "牛牛","Niu Niu"));
-        list.add(setData("106", "三公","Three Face"));
-        list.add(setData("107", "番摊","Fantan"));
-        list.add(setData("108", "色碟","Se Die"));
-        list.add(setData("110", "鱼虾蟹","Fish-Prawn-Crab"));
-        list.add(setData("111", "炸金花","Golden Flower"));
-        list.add(setData("112", "温州牌九","Wenzhou Pai Gow"));
-        list.add(setData("113", "二八杠","Mahjong tiles"));
-        list.add(setData("128", "安達巴哈","AndarBahar"));
-        washCodeConfigService.saveAll(list);
+        list.add(setData("101", "百家乐", "Baccarat"));
+        list.add(setData("102", "龙虎", "Dragon Tiger"));
+        list.add(setData("103", "轮盘", "Roulette"));
+        list.add(setData("104", "骰宝", "SicBo"));
+        list.add(setData("105", "牛牛", "Niu Niu"));
+        list.add(setData("106", "三公", "Three Face"));
+        list.add(setData("107", "番摊", "Fantan"));
+        list.add(setData("108", "色碟", "Se Die"));
+        list.add(setData("110", "鱼虾蟹", "Fish-Prawn-Crab"));
+        list.add(setData("111", "炸金花", "Golden Flower"));
+        list.add(setData("112", "温州牌九", "Wenzhou Pai Gow"));
+        list.add(setData("113", "二八杠", "Mahjong tiles"));
+        list.add(setData("128", "安達巴哈", "AndarBahar"));
+        list.add(setData("201", "PG", "PG"));
+        list.add(setData("202", "CQ9", "CQ9"));
+        return list;
     }
 
-    private WashCodeConfig setData(String gameId, String gameName,String gameEnName) {
+    private WashCodeConfig setData(String gameId, String gameName, String gameEnName) {
         WashCodeConfig codeConfig = new WashCodeConfig();
         codeConfig.setPlatform(Constants.PLATFORM);
         codeConfig.setGameId(gameId);
