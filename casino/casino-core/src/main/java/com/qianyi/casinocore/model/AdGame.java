175ba2d25647c04cd16ffbebbbb679dce61d3ed1
benson <bb0491742@gmail.com>
2022-02-12 20:08:53 +0800

    游戏新增英文名称字段

---
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/AdGame.java
@@ -0,0 +1,28 @@
+package com.qianyi.casinocore.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@ApiModel("游戏表")
+public class AdGame extends BaseEntity{
+
+    @ApiModelProperty(value = "游戏平台ID")
+    private Integer gamePlatformId;
+
+    @ApiModelProperty(value = "游戏编码")
+    private String gameCode;
+
+    @ApiModelProperty(value = "游戏中文名称")
+    private String gameName;
+
+    @ApiModelProperty(value = "游戏英文名称")
+    private String gameEnName;
+
+    @ApiModelProperty(value = "是否维护 0：维护 1正常")
+    private Integer gamesStatus;
+}
