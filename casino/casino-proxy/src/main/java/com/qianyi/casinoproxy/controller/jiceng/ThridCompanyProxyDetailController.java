bb3d12020495174dde6631b45e9914729792508d
benson <bb0491742@gmail.com>
2022-02-13 19:20:39 +0800

    编译报错暂时注释处理

--- casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/jiceng/ThridCompanyProxyDetailController.java
+++ casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/jiceng/ThridCompanyProxyDetailController.java
@@ -171,7 +171,8 @@ public class ThridCompanyProxyDetailController {
         Date end = DateUtil.getSimpleDateFormat().parse(endTime);
         proxyHomePageReportService.chargeOrder(byId, start, end, proxyHomePageReport);
         proxyHomePageReportService.withdrawOrder(byId, start, end, proxyHomePageReport);
-        Set<Long> set = proxyHomePageReportService.gameRecordAndActive(byId, startTime, endTime, proxyHomePageReport);
+//        Set<Long> set = proxyHomePageReportService.gameRecordAndActive(byId, startTime, endTime, proxyHomePageReport);
+        Set<Long> set = new HashSet<>();
         proxyHomePageReportService.getNewUsers(byId, start, end, proxyHomePageReport);
         CompanyProxyReportVo companyProxyReportVo = this.assemble(proxyHomePageReport,set);
         return this.assemble(companyProxyReportVo,byId);
