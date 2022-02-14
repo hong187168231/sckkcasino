4a840eed3eae832a3f5af1d961a7e9dfdf8fcf72
zhenhao-java <zhenhao.java@gmail.com>
2022-02-14 20:41:20 +0800

    修改会员总报表

--- casino/casino-core/src/main/java/com/qianyi/casinocore/service/UserService.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/service/UserService.java
@@ -6,6 +6,7 @@ import com.qianyi.casinocore.model.User;
 import com.qianyi.casinocore.model.UserMoney;
 import com.qianyi.casinocore.repository.UserRepository;
 import com.qianyi.casinocore.util.CommonConst;
+import com.qianyi.casinocore.util.SqlConst;
 import com.qianyi.modulecommon.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.*;
@@ -259,30 +260,9 @@ public class UserService {
     @SuppressWarnings("unchecked")
     public List<Map<String,Object>> findMap(String startTime,String endTime,Integer page,Integer pageSize,String sort)
         throws Exception {
-        String dataSql = "select \n" + "u.account , \n" + "u.third_proxy , \n" + " u.id,\n"
-            + " ifnull(main_t.num,0) num,\n" + " ifnull(main_t.bet_amount,0) bet_amount ,\n"
-            + " ifnull(main_t.validbet,0) validbet ,\n" + " ifnull(main_t.win_loss,0) win_loss ,\n"
-            + " ifnull(wash_t.wash_amount,0) wash_amount, \n"
-            + " ifnull(withdraw_t.service_charge,0) service_charge, \n" + " ifnull(pr.amount,0) all_profit_amount, \n"
-            + " -(ifnull(main_t.win_loss,0)+ifnull(wash_t.wash_amount,0)) avg_benefit, \n"
-            + " -(ifnull(main_t.win_loss,0)+ifnull(wash_t.wash_amount,0))-ifnull(pr.amount,0)+ifnull(withdraw_t.service_charge,0) total_amount\n"
-            + "from user u\n" + "left join ( \n" + "  select user_id , \n" + "  count(1) num, \n"
-            + "  sum(bet) bet_amount, \n" + "  sum(validbet) validbet , \n" + "  sum(win_loss) win_loss  \n"
-            + "  from game_record gr \n"
-            + "  where settime >= {0} and settime <= {1}\n"
-            + "  group by user_id \n" + " ) main_t on u.id = main_t.user_id\n" + " left join ( \n"
-            + "  select user_id , sum(amount) wash_amount  \n" + "  from wash_code_change wcc  \n"
-            + "  where create_time >= {0} and create_time <= {1}\n"
-            + "  group by user_id \n" + " ) wash_t on u.id = wash_t.user_id\n" + " left join ( \n"
-            + "  select user_id , sum(ifnull(service_charge,0)) service_charge  \n" + "  from withdraw_order wo  \n"
-            + "  where update_time >= {0} and update_time <= {1}\n"
-            + "  group by user_id \n" + " ) withdraw_t on u.id = withdraw_t.user_id\n" + " left join ( \n"
-            + "  select user_id , sum(amount) amount from share_profit_change spc  \n"
-            + "  where create_time >= {0} and create_time <= {1}\n"
-            + "  group by user_id  \n" + " ) pr on u.id=pr.user_id \n" + " where 1=1 {2} \n" + "limit {3},{4}";
         startTime = "'"+startTime+"'";
         endTime = "'"+endTime+"'";
-        String sql = MessageFormat.format(dataSql,startTime,endTime,sort,page.toString(),pageSize.toString());
+        String sql = MessageFormat.format(SqlConst.dataSql,startTime,endTime,sort,page.toString(),pageSize.toString());
         System.out.println(sql);
         Query countQuery = entityManager.createNativeQuery(sql);
         List resultList = countQuery.getResultList();
