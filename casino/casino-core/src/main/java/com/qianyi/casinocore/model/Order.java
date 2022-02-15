9c90e920710b4e5c57f4f76d3b4e05d2ffe38b9f
“tony117269” <tony1172692857@gmail.com>
2022-02-15 15:31:23 +0800

    Order表添加字段

--- casino/casino-core/src/main/java/com/qianyi/casinocore/model/Order.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/Order.java
@@ -28,6 +28,10 @@ public class Order extends BaseEntity {
     @Column(columnDefinition = "varchar(16) default 'WM'")
     private String gamePlatformName;
 
+    @ApiModelProperty("平台名称: wm, PG/CQ9")
+    @Column(columnDefinition = "varchar(16) default 'WM'")
+    private String gamePlatformName;
+
     /**
      * 0.转入，1.转出
      */
