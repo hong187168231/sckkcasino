02830759efd839c7632ac55a1158d94acc39ac54
kate <kate98765@gmail.com>
2022-02-15 11:43:44 +0800

    提交拉取GoldenF注单

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/GameRecordGoldenfEndTimeRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/GameRecordGoldenfEndTimeRepository.java
@@ -1,5 +1,6 @@
 package com.qianyi.casinocore.repository;
 
+import com.qianyi.casinocore.model.GameRecordGoldenF;
 import com.qianyi.casinocore.model.GameRecordGoldenfEndTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -8,4 +9,6 @@ public interface GameRecordGoldenfEndTimeRepository extends JpaRepository<GameRe
 
 //    GameRecordGoldenfEndTime findFirstByOrderByEndTimeDesc();
     GameRecordGoldenfEndTime findFirstByOrderByEndTimeDesc();
+
+    GameRecordGoldenfEndTime findFirstByVendorCodeOrderByEndTimeDesc(String vendor);
 }
