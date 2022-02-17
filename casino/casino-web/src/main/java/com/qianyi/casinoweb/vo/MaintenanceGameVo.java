df0afc39df54d80c15e48df69cb18b12661d0393
benson <bb0491742@gmail.com>
2022-02-17 22:59:35 +0800

    字段加注释

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/vo/MaintenanceGameVo.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/vo/MaintenanceGameVo.java
@@ -14,7 +14,7 @@ public class MaintenanceGameVo {
     @ApiModelProperty(value = "平台:WM,PG,CQ9")
     private String gamePlatformName;
 
-    @ApiModelProperty(value = "平台状态：0：维护，1：正常")
+    @ApiModelProperty(value = "平台状态：0：维护，1：正常，2：关闭")
     private Integer platformStatus;
 
     @ApiModelProperty(value = "维护/关闭状态游戏列表")
