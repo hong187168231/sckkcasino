5a3a97910cf520b23d316068b7a29c967cbe6361
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-02-21 17:21:04 +0700

    合并代码

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/GameRecordRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/GameRecordRepository.java
@@ -45,53 +45,53 @@ public interface GameRecordRepository extends JpaRepository<GameRecord, Long>, J
             "ifnull(g.second_proxy,0) second_proxy,ifnull(g.third_proxy,0) third_proxy," +
             "g.gid gid,COUNT(1) num,SUM(g.bet) bet,SUM(g.validbet) validbet,SUM(g.win_loss) win_loss," +
             " ifnull(SUM(w.amount),0) amount from game_record g left join  " +
-            "wash_code_change w  on  w.game_record_id = g.id where g.settime >= ?1 " +
-            "and g.settime <= ?2 GROUP BY g.third_proxy,g.gid  ",nativeQuery = true)
+            "wash_code_change w  on  w.game_record_id = g.id where g.bet_time >= ?1 " +
+            "and g.bet_time <= ?2 GROUP BY g.third_proxy,g.gid  ",nativeQuery = true)
     List<Map<String,Object>> queryGameRecords(String startTime,String endTime);
 
-    @Query(value = "select LEFT(g.settime,?3) set_time,ifnull(g.first_proxy,0) first_proxy," +
-        "ifnull(g.second_proxy,0) second_proxy,ifnull(g.third_proxy,0) third_proxy," +
-        "g.gid gid,COUNT(1) num,SUM(g.bet) bet,SUM(g.validbet) validbet,SUM(g.win_loss) win_loss," +
-        " ifnull(SUM(w.amount),0) amount from game_record g left join  " +
-        "wash_code_change w  on  w.game_record_id = g.id where g.create_time >= ?1 " +
-        "and g.create_time <= ?2 GROUP BY g.third_proxy,g.gid,LEFT(g.settime,?3)  ",nativeQuery = true)
+    @Query(value = "select LEFT(g.bet_time,?3) set_time,ifnull(g.first_proxy,0) first_proxy," +
+            "ifnull(g.second_proxy,0) second_proxy,ifnull(g.third_proxy,0) third_proxy," +
+            "g.gid gid,COUNT(1) num,SUM(g.bet) bet,SUM(g.validbet) validbet,SUM(g.win_loss) win_loss," +
+            " ifnull(SUM(w.amount),0) amount from game_record g left join  " +
+            "wash_code_change w  on  w.game_record_id = g.id where g.create_time >= ?1 " +
+            "and g.create_time <= ?2 GROUP BY g.third_proxy,g.gid,LEFT(g.bet_time,?3)  ",nativeQuery = true)
     List<Map<String,Object>> queryGameRecords(String startTime,String endTime,Integer num);
 
-    @Query(value = "select MAX(g.id) maxId,LEFT(g.settime,?2) set_time,ifnull(g.first_proxy,0) first_proxy," +
-        "ifnull(g.second_proxy,0) second_proxy,ifnull(g.third_proxy,0) third_proxy," +
-        "g.gid gid,COUNT(1) num,SUM(g.bet) bet,SUM(g.validbet) validbet,SUM(g.win_loss) win_loss," +
-        " ifnull(SUM(w.amount),0) amount from game_record g left join  " +
-        "wash_code_change w  on  w.game_record_id = g.id and w.platform = 'wm' where g.id > ?1 " +
-        " GROUP BY g.third_proxy , LEFT(g.settime,?2)  ",nativeQuery = true)
+    @Query(value = "select MAX(g.id) maxId,LEFT(g.bet_time,?2) set_time,ifnull(g.first_proxy,0) first_proxy," +
+            "ifnull(g.second_proxy,0) second_proxy,ifnull(g.third_proxy,0) third_proxy," +
+            "g.gid gid,COUNT(1) num,SUM(g.bet) bet,SUM(g.validbet) validbet,SUM(g.win_loss) win_loss," +
+            " ifnull(SUM(w.amount),0) amount from game_record g left join  " +
+            "wash_code_change w  on  w.game_record_id = g.id and w.platform = 'wm' where g.id > ?1 " +
+            " GROUP BY g.third_proxy , LEFT(g.bet_time,?2)  ",nativeQuery = true)
     List<Map<String,Object>> queryGameRecords(Long id,Integer num);
 
-    @Query(value = "select g.user_id userId,SUM(g.validbet) validbet from game_record g where g.settime BETWEEN ?1 and ?2 GROUP BY g.user_id;",nativeQuery = true)
+    @Query(value = "select g.user_id userId,SUM(g.validbet) validbet from game_record g where g.bet_time BETWEEN ?1 and ?2 GROUP BY g.user_id;",nativeQuery = true)
     List<Map<String, Object>> findGameRecords(String startTime,String endTime);
 
-    @Query(value = "select SUM(g.validbet) validbet from game_record g where g.user_id = ?1 and g.settime BETWEEN ?2 and ?3 ",nativeQuery = true)
+    @Query(value = "select SUM(g.validbet) validbet from game_record g where g.user_id = ?1 and g.bet_time BETWEEN ?2 and ?3 ",nativeQuery = true)
     BigDecimal findGameRecords(Long userId,String startTime,String endTime);
 
-    @Query(value = "select g.user_id  from game_record g where g.settime BETWEEN ?1 and ?2 GROUP BY g.user_id;",nativeQuery = true)
+    @Query(value = "select g.user_id  from game_record g where g.bet_time BETWEEN ?1 and ?2 GROUP BY g.user_id;",nativeQuery = true)
     Set<Long> findGroupByUser(String startTime,String endTime);
 
-    @Query(value = "select SUM(g.validbet) validbet,SUM(g.win_loss) winLoss from game_record g where g.settime BETWEEN ?1 and ?2 ",nativeQuery = true)
+    @Query(value = "select SUM(g.validbet) validbet,SUM(g.win_loss) winLoss from game_record g where g.bet_time BETWEEN ?1 and ?2 ",nativeQuery = true)
     Map<String, Object> findSumBetAndWinLoss(String startTime,String endTime);
 
-    @Query(value = "select g.user_id  from game_record g where g.settime BETWEEN ?1 and ?2 and g.first_proxy = ?3 GROUP BY g.user_id;",nativeQuery = true)
+    @Query(value = "select g.user_id  from game_record g where g.bet_time BETWEEN ?1 and ?2 and g.first_proxy = ?3 GROUP BY g.user_id;",nativeQuery = true)
     Set<Long> findGroupByFirst(String startTime,String endTime,Long firstProxy);
 
-    @Query(value = "select ifnull(SUM(g.validbet),0) validbet,ifnull(SUM(g.win_loss),0) winLoss from game_record g where g.settime BETWEEN ?1 and ?2 and g.first_proxy = ?3",nativeQuery = true)
+    @Query(value = "select ifnull(SUM(g.validbet),0) validbet,ifnull(SUM(g.win_loss),0) winLoss from game_record g where g.bet_time BETWEEN ?1 and ?2 and g.first_proxy = ?3",nativeQuery = true)
     Map<String, Object> findSumBetAndWinLossByFirst(String startTime,String endTime,Long firstProxy);
 
-    @Query(value = "select g.user_id  from game_record g where g.settime BETWEEN ?1 and ?2 and g.second_proxy = ?3 GROUP BY g.user_id;",nativeQuery = true)
+    @Query(value = "select g.user_id  from game_record g where g.bet_time BETWEEN ?1 and ?2 and g.second_proxy = ?3 GROUP BY g.user_id;",nativeQuery = true)
     Set<Long> findGroupBySecond(String startTime,String endTime,Long secondProxy);
 
-    @Query(value = "select ifnull(SUM(g.validbet),0) validbet,ifnull(SUM(g.win_loss),0) winLoss  from game_record g where g.settime BETWEEN ?1 and ?2 and g.second_proxy = ?3",nativeQuery = true)
+    @Query(value = "select ifnull(SUM(g.validbet),0) validbet,ifnull(SUM(g.win_loss),0) winLoss  from game_record g where g.bet_time BETWEEN ?1 and ?2 and g.second_proxy = ?3",nativeQuery = true)
     Map<String, Object> findSumBetAndWinLossBySecond(String startTime,String endTime,Long secondProxy);
 
-    @Query(value = "select g.user_id  from game_record g where g.settime BETWEEN ?1 and ?2 and g.third_proxy = ?3 GROUP BY g.user_id;",nativeQuery = true)
+    @Query(value = "select g.user_id  from game_record g where g.bet_time BETWEEN ?1 and ?2 and g.third_proxy = ?3 GROUP BY g.user_id;",nativeQuery = true)
     Set<Long> findGroupByThird(String startTime,String endTime,Long thirdProxy);
 
-    @Query(value = "select ifnull(SUM(g.validbet),0) validbet,ifnull(SUM(g.win_loss),0) winLoss  from game_record g where g.settime BETWEEN ?1 and ?2 and g.third_proxy = ?3",nativeQuery = true)
+    @Query(value = "select ifnull(SUM(g.validbet),0) validbet,ifnull(SUM(g.win_loss),0) winLoss  from game_record g where g.bet_time BETWEEN ?1 and ?2 and g.third_proxy = ?3",nativeQuery = true)
     Map<String, Object> findSumBetAndWinLossByThird(String startTime,String endTime,Long thirdProxy);
 }
