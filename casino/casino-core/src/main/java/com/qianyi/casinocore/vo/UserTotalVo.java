4277e8a9c3232e1ed68161a869d0cc002a557813
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-02-23 13:01:10 +0700

    总计待领取洗码保留2位小数

--- casino/casino-core/src/main/java/com/qianyi/casinocore/vo/UserTotalVo.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/vo/UserTotalVo.java
@@ -12,9 +12,9 @@ import java.math.BigDecimal;
 public class UserTotalVo implements Serializable {
     private static final long serialVersionUID = -6875617998456387632L;
     @ApiModelProperty("待领取洗码金额")
+    @JsonSerialize(using = Decimal2Serializer.class, nullsUsing = Decimal2Serializer.class)
     private BigDecimal washCode = BigDecimal.ZERO;
     @ApiModelProperty("中心余额")
-    @JsonSerialize(using = Decimal2Serializer.class, nullsUsing = Decimal2Serializer.class)
     private BigDecimal money = BigDecimal.ZERO;
 
 }
