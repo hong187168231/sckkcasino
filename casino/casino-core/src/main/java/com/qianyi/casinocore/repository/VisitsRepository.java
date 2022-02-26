6391d8d8e894e0ead338eec481db9afedf70a1c1
“tony117269” <tony1172692857@gmail.com>
2022-02-26 15:14:16 +0800

    域名管理

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/VisitsRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/VisitsRepository.java
@@ -3,6 +3,16 @@ package com.qianyi.casinocore.repository;
 import com.qianyi.casinocore.model.Visits;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
 
 public interface VisitsRepository extends JpaRepository<Visits,Long>, JpaSpecificationExecutor<Visits> {
+
+    @Query(value = "select domain_name as domainName , ip as ip, count(0) as domainCount  from visits " +
+            "where create_time between ?1 and ?2 " +
+            "and if(?3!='', domain_name =?3, 1=1) " +
+            "and if(?4!='', ip =?4, 1=1) GROUP BY domain_name , ip", nativeQuery = true)
+    List<Map<String,Object>> findListGroupBy(String startTime, String endTime, String domainName, String ip);
 }
--- casino/casino-core/src/main/java/com/qianyi/casinocore/service/VisitsService.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/service/VisitsService.java
@@ -5,6 +5,9 @@ import com.qianyi.casinocore.repository.VisitsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class VisitsService {
 
@@ -13,5 +16,9 @@ public class VisitsService {
 
     public void save(Visits visits) {
         visitsRepository.save(visits);
+    }
+
+    public List<Map<String,Object>> findListSum(String domainName, String ip, String StartTime, String endTime) {
+        return visitsRepository.findListGroupBy(StartTime, endTime, domainName, ip);
     }
 }
