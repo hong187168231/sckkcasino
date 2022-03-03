9f4a1c7e92e348dfe3344494748b1eb49f44f9ee
benson <bb0491742@gmail.com>
2022-03-03 13:14:13 +0800

    新增马来语翻译

--- module-common/src/main/java/com/qianyi/modulecommon/util/MessageUtil.java
+++ module-common/src/main/java/com/qianyi/modulecommon/util/MessageUtil.java
@@ -27,17 +27,7 @@ public class MessageUtil {
      */
     public String get(String msgKey) {
         try {
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-            String language = request.getHeader(Constants.LANGUAGE);
-            //默认中文
-            Locale locale = Locale.CHINA;
-            //柬埔寨语
-            Locale km_kh = new Locale("km", "KH");
-            if (Locale.US.toString().equals(language)) {
-                locale = Locale.US;
-            } else if (km_kh.toString().equals(language)) {
-                locale = km_kh;
-            }
+            Locale locale = getLocale();
 //            Locale locale = LocaleContextHolder.getLocale();
             //以中文为key,中文翻译可以不配，直接取key
             if (locale.equals(Locale.CHINA)) {
@@ -50,5 +40,24 @@ public class MessageUtil {
             //e.printStackTrace();
             return msgKey;
         }
+    }
+
+    private Locale getLocale() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String language = request.getHeader(Constants.LANGUAGE);
+        //默认中文
+        Locale locale = Locale.CHINA;
+        //柬埔寨语
+        Locale km_kh = new Locale("km", "KH");
+        //马来西亚语
+        Locale as_MY = new Locale("as", "MY");
+        if (Locale.US.toString().equals(language)) {
+            locale = Locale.US;
+        } else if (km_kh.toString().equals(language)) {
+            locale = km_kh;
+        } else if (as_MY.toString().equals(language)) {
+            locale = as_MY;
+        }
+        return locale;
     }
 }
