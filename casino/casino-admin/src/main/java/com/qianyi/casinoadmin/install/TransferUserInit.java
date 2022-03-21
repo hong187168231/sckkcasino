c916fdc6cd9b92d125610306d1121349d9804e6e
zhenhao-java <zhenhao.java@gmail.com>
2022-03-21 14:19:49 +0800

    修改转移会员

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/TransferUserInit.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/TransferUserInit.java
@@ -28,37 +28,37 @@ public class TransferUserInit implements CommandLineRunner {
     private static List<String> users = new ArrayList<>();
 
     static {
-        users.add("NankD018");
-        users.add("MengD018");
-        users.add("Makara911");
-        users.add("Kimhong009");
-        users.add("VANNDA33");
-        users.add("MNUSSMOS33");
-        users.add("HAILENG18");
-        users.add("pozzmab186");
-        users.add("NAKA99");
-        users.add("PHALLARA555");
-        users.add("Dornty888");
-        users.add("BONGPOV168");
-        users.add("Chhay888");
-        users.add("kimsreang522");
-        users.add("HUYVANARITH55");
-        users.add("ChanChan003");
-        users.add("MrrHout001");
-        users.add("Dano9595");
-        users.add("NopNop002");
-        users.add("Rith168");
-        users.add("BongNang777");
-        users.add("HeaTen002");
-        users.add("Neang999");
-        users.add("TeTe777");
-        users.add("Bongda777");
+        users.add("Makly855");
+        users.add("Rahu855");
+//        users.add("Makara911");
+//        users.add("Kimhong009");
+//        users.add("VANNDA33");
+//        users.add("MNUSSMOS33");
+//        users.add("HAILENG18");
+//        users.add("pozzmab186");
+//        users.add("NAKA99");
+//        users.add("PHALLARA555");
+//        users.add("Dornty888");
+//        users.add("BONGPOV168");
+//        users.add("Chhay888");
+//        users.add("kimsreang522");
+//        users.add("HUYVANARITH55");
+//        users.add("ChanChan003");
+//        users.add("MrrHout001");
+//        users.add("Dano9595");
+//        users.add("NopNop002");
+//        users.add("Rith168");
+//        users.add("BongNang777");
+//        users.add("HeaTen002");
+//        users.add("Neang999");
+//        users.add("TeTe777");
+//        users.add("Bongda777");
     }
 
     @Override
     public void run(String... args) throws Exception {
         log.info("初始化转移会员开始============================================》");
-        ProxyUser d018JD = proxyUserService.findByUserName("D018JD");
+        ProxyUser d018JD = proxyUserService.findByUserName("L037JD");
         if (LoginUtil.checkNull(d018JD)){
             log.info("没有找到这个代理转移结束");
             return;
