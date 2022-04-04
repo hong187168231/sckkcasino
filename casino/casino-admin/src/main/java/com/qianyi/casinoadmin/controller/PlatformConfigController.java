d377bdfc2f83c30e1db918e24a2250ae05e2df92
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-04-04 20:31:54 +0700

    编辑图片服务器修改

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/PlatformConfigController.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/controller/PlatformConfigController.java
@@ -341,7 +341,7 @@ public class PlatformConfigController {
         if (LoginUtil.checkNull(first)){
             first = new PlatformConfig();
         }
-        if(first.getUploadUrl().equals(uploadUrl)){
+        if(first.getUploadUrl()!=null && first.getUploadUrl().equals(uploadUrl)){
             return ResponseUtil.custom("当前地址相同,请勿重复修改");
         }
         first.setUploadUrl(uploadUrl);
@@ -388,7 +388,7 @@ public class PlatformConfigController {
         if (LoginUtil.checkNull(first)){
             first = new PlatformConfig();
         }
-        if(first.getReadUploadUrl().equals(uploadUrl)){
+        if(first.getReadUploadUrl()!=null && first.getReadUploadUrl().equals(uploadUrl)){
             return ResponseUtil.custom("当前地址相同,请勿重复修改");
         }
         first.setReadUploadUrl(uploadUrl);
