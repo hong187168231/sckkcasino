b7b1a1af100e4b32739430da5536a2a1ccc9b238
Lulu13695 <94361337+Lulu13695@users.noreply.github.com>
2022-04-07 19:57:26 +0700

    代理对账报表增加返利逻辑修改

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/GameRecordReportNewRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/GameRecordReportNewRepository.java
@@ -14,7 +14,7 @@ public interface GameRecordReportNewRepository  extends JpaRepository<GameRecord
     @Query(value = "INSERT INTO game_record_report_new (game_record_report_id,statics_times,bet_amount,valid_amount,"
         + "win_loss_amount,amount,betting_number,first_proxy,second_proxy,third_proxy,platform,surplus_amount,user_amount) " +
         "VALUES (?1,?2,?3,?4,?5,?6,?7,?8,?9,?10,?11,?12,?13) ON DUPLICATE KEY UPDATE bet_amount=bet_amount + ?3,"
-        + "valid_amount=valid_amount + ?4,win_loss_amount=win_loss_amount + ?5,amount=amount + ?6,betting_number=betting_number + ?7 ;",nativeQuery = true)
+        + "valid_amount=valid_amount + ?4,win_loss_amount=win_loss_amount + ?5,amount=amount + ?6,betting_number=betting_number + ?7 ,surplus_amount=surplus_amount + ?12,user_amount=user_amount + ?13 ;",nativeQuery = true)
     void updateKey(Long gameRecordReportId,String staticsTimes,BigDecimal betAmount,BigDecimal validAmount,BigDecimal winLossAmount,
         BigDecimal amount,Integer bettingNumber,Long firstProxy,Long secondProxy,Long thirdProxy,String platform,BigDecimal surplusAmount,BigDecimal userAmount);
 
