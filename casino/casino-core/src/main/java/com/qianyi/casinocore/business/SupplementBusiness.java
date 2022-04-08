765f79967ab322e86d35fcc67bdb1e2a196deb53
benson <bb0491742@gmail.com>
2022-04-08 15:15:16 +0800

    异常订单自动补单完善

--- casino/casino-core/src/main/java/com/qianyi/casinocore/business/SupplementBusiness.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/business/SupplementBusiness.java
@@ -45,7 +45,7 @@ public class SupplementBusiness {
         while (true) {
             try {
                 if (requestNum >= 3) {
-                    log.error("尝试3次补单失败,errorOrder={}",errorOrder.toString());
+                    log.error("尝试3次补单失败,errorOrder={}", errorOrder.toString());
                     break;
                 }
                 requestNum++;
@@ -58,23 +58,49 @@ public class SupplementBusiness {
                 }
                 if (entity.getErrorCode() == 107) {
                     log.info("订单号:{}查询无记录", orderNo);
-                    addMoneySaveAccountChange(errorOrder, errorOrder.getMoney(), orderNo);
+                    //转入wm时，wm查询无记录说明wm加点失败，要把本地的钱加回来，
+                    if (errorOrder.getType() == AccountChangeEnum.WM_IN.getType()) {
+                        //更新错误订单表状态
+                        updateErrorOrderStatus(errorOrder, "自动补单成功，补单金额:" + errorOrder.getMoney() + ",转入WM时加点失败,加回本地额度");
+                        //加回额度
+                        addMoney(errorOrder.getUserId(), errorOrder.getMoney());
+                        //记录账变
+                        saveAccountChange(errorOrder, errorOrder.getMoney());
+                    } else if (errorOrder.getType() == AccountChangeEnum.RECOVERY.getType()) {
+                        //转出wm时，是先扣减wm的钱再加回本地，wm查询无记录说明没有扣点成功，本地也不用把钱加回来,更新状态就行
+                        updateErrorOrderStatus(errorOrder, "自动补单成功，补单金额:0,转出WM时扣点失败,额度未丢失");
+                    }
                     break;
                 }
                 if (entity.getErrorCode() != 0) {
                     log.info("订单号:{}查询交易记录异常,result={}", orderNo, entity.toString());
+                    continue;
+                }
+                if (entity.getErrorCode() == 0) {
+                    String result = entity.getResult().toString();
+                    List<WmMemberTradeReportVo> records = JSON.parseArray(result, WmMemberTradeReportVo.class);
+                    for (WmMemberTradeReportVo vo : records) {
+                        if (thirdAccount.equals(vo.getUser()) && orderNo.equals(vo.getOrdernum())) {
+                            //转入wm,本地先扣减，确认三方加点成功无需加回本地余额
+                            if (vo.getOp_code() == 121 && errorOrder.getType() == AccountChangeEnum.WM_IN.getType()) {
+                                updateErrorOrderStatus(errorOrder, "自动补单成功，补单金额:0,转入WM时加点成功,额度未丢失");
+                                break;
+                                //转出WM时，三方先扣减，确认三方扣点成功加回本地余额
+                            } else if (vo.getOp_code() == 122 && errorOrder.getType() == AccountChangeEnum.RECOVERY.getType()) {
+                                //以WM额度为准
+                                BigDecimal money = new BigDecimal(vo.getMoney()).abs();
+                                //更新错误订单表状态
+                                updateErrorOrderStatus(errorOrder, "自动补单成功，补单金额:" + money + ",转出WM时扣点成功,加回本地额度");
+                                //加回额度
+                                addMoney(errorOrder.getUserId(), money);
+                                //记录账变
+                                saveAccountChange(errorOrder, money);
+                                break;
+                            }
+                        }
+                    }
                     break;
                 }
-                String result = entity.getResult().toString();
-                List<WmMemberTradeReportVo> records = JSON.parseArray(result, WmMemberTradeReportVo.class);
-
-                for (WmMemberTradeReportVo vo : records) {
-                    if (thirdAccount.equals(vo.getUser()) && orderNo.equals(vo.getOrdernum())) {
-                        addMoneySaveAccountChange(errorOrder, new BigDecimal(vo.getMoney()), orderNo);
-                        break;
-                    }
-                }
-                break;
             } catch (Exception e) {
                 e.printStackTrace();
                 log.error("查询WM交易记录时异常,msg={}", e.getMessage());
@@ -82,23 +108,28 @@ public class SupplementBusiness {
         }
     }
 
-    public void addMoneySaveAccountChange(ErrorOrder errorOrder,BigDecimal money,String orderNo){
-        BigDecimal amount = money.abs();
-        userMoneyService.addMoney(errorOrder.getUserId(), amount);
-        log.info("userMoney表金额补偿成功,userId={},money={}", errorOrder.getUserId(), amount);
+    public void addMoney(Long userId, BigDecimal money) {
+        userMoneyService.addMoney(userId, money);
+        log.info("订单自动补单成功,userMoney表金额补偿成功,userId={},money={}", userId, money);
+    }
+
+    public void updateErrorOrderStatus(ErrorOrder errorOrder, String remark) {
         errorOrder.setStatus(1);
-        errorOrder.setRemark("自动补单成功,补单金额:" + amount);
+        errorOrder.setRemark(remark);
         errorOrderRepository.save(errorOrder);
         log.info("订单自动补单成功,errorOrder表更新成功,errorOrder={}", errorOrder.toString());
+    }
+
+    public void saveAccountChange(ErrorOrder errorOrder, BigDecimal money) {
         //账变中心记录账变
         UserMoney userMoney = userMoneyService.findByUserId(errorOrder.getUserId());
         AccountChangeVo accountChangeVo = new AccountChangeVo();
         accountChangeVo.setUserId(errorOrder.getUserId());
         accountChangeVo.setChangeEnum(AccountChangeEnum.SYSTEM_UPP);
-        accountChangeVo.setAmount(amount);
-        accountChangeVo.setAmountBefore(userMoney.getMoney());
-        accountChangeVo.setAmountAfter(userMoney.getMoney().subtract(amount));
-        accountChangeVo.setOrderNo(orderNo);
+        accountChangeVo.setAmount(money);
+        accountChangeVo.setAmountAfter(userMoney.getMoney());
+        accountChangeVo.setAmountBefore(userMoney.getMoney().subtract(money));
+        accountChangeVo.setOrderNo(errorOrder.getOrderNo());
         asyncService.executeAsync(accountChangeVo);
         log.info("订单自动补单成功,AccountChange表账变记录成功,AccountChange={}", accountChangeVo.toString());
     }
