c0f8dc2071c1d275f13e9adaec0f418bd1f5d4b9
benson <bb0491742@gmail.com>
2022-04-20 15:12:46 +0800

    OB注单表

---
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/model/GameRecordObTournament.java
@@ -0,0 +1,23 @@
+package com.qianyi.casinocore.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name ="game_record_ob_tournament")
+@ApiModel("OB游戏记录锦标赛")
+public class GameRecordObTournament extends BaseEntity{
+
+    @ApiModelProperty(value = "赛事ID")
+    private String matchId;
+
+    @ApiModelProperty(value = "赛事简称")
+    private String matchName;
+}
