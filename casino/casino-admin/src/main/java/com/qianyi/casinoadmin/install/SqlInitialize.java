9152a05836b2f0a4b77e7695b418c8d438711d45
zhenhao-java <zhenhao.java@gmail.com>
2022-07-05 14:04:48 +0800

    初始化计算沙巴体育

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/SqlInitialize.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/install/SqlInitialize.java
@@ -1,12 +1,8 @@
 package com.qianyi.casinoadmin.install;
 
-import com.qianyi.casinoadmin.util.LoginUtil;
 import com.qianyi.casinocore.model.GameRecordEndIndex;
-import com.qianyi.casinocore.model.SysPermission;
 import com.qianyi.casinocore.service.GameRecordEndIndexService;
 import com.qianyi.casinocore.service.GameRecordReportNewService;
-import com.qianyi.casinocore.service.SysPermissionService;
-import com.qianyi.casinocore.service.WithdrawOrderService;
 import com.qianyi.modulecommon.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +14,6 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Order(4)
 public class SqlInitialize  implements CommandLineRunner {
-    //    @Autowired
-    //    private SysPermissionService sysPermissionService;
-
-    //    @Autowired
-    //    private WithdrawOrderService withdrawOrderService;
-
     @Autowired
     private GameRecordEndIndexService gameRecordEndIndexService;
 
@@ -32,22 +22,10 @@ public class SqlInitialize  implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        //        SysPermission sysPermission1 = sysPermissionService.findByName("历史盈亏报表");
-        //        if (!LoginUtil.checkNull(sysPermission1)){
-        //            sysPermission1.setName("会员报表");
-        //            sysPermissionService.save(sysPermission1);
-        //        }
-        //        SysPermission sysPermission2 =sysPermissionService.findByName("代理实时报表");
-        //        if (!LoginUtil.checkNull(sysPermission2)){
-        //            sysPermission2.setName("代理报表");
-        //            sysPermissionService.save(sysPermission2);
-        //        }
-        //        withdrawOrderService.updateWithdrawOrderAuditId(0L);
-
-//        GameRecordEndIndex first = gameRecordEndIndexService.findUGameRecordEndIndexUseLock();
-//        first.setSABASPORTMaxId(0L);
-//        gameRecordEndIndexService.save(first);
-//        gameRecordReportNewService.deleteByPlatform(Constants.PLATFORM_SABASPORT);
-//        gameRecordReportNewService.saveGameRecordReportSABASPORT();
+        GameRecordEndIndex first = gameRecordEndIndexService.findUGameRecordEndIndexUseLock();
+        first.setSABASPORTMaxId(0L);
+        gameRecordEndIndexService.save(first);
+        gameRecordReportNewService.deleteByPlatform(Constants.PLATFORM_SABASPORT);
+        gameRecordReportNewService.saveGameRecordReportSABASPORT();
     }
 }
