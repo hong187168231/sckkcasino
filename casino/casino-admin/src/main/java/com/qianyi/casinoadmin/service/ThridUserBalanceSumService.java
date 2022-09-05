86e4fc2fc10d0fd27de8f52a86d972b34abd4098
“tony117269” <tony1172692857@gmail.com>
2022-09-05 13:26:29 +0800

    沙巴体育查询总余额

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/service/ThridUserBalanceSumService.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/service/ThridUserBalanceSumService.java
@@ -42,7 +42,7 @@ public class ThridUserBalanceSumService {
         for (UserThird u:sabaThird){
             threadPool.execute(() ->{
                 try {
-                    JSONObject jsonObject = userMoneyService.refreshSABAUserId(u.getUserId().toString());
+                    JSONObject jsonObject = userMoneyService.refreshSABA(u.getUserId());
 
                     if (LoginUtil.checkNull(jsonObject) || LoginUtil.checkNull(jsonObject.get("code"),jsonObject.get("msg"))){
                         list.add(BigDecimal.ZERO);
