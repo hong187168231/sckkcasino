ab24cea1af169b6841697ca2de6a8b54abc573e0
puff <yinpuff@gmail.com>
2022-12-24 14:34:17 +0800

    修改代理后台类型转换

--- casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/CodeNumChangeController.java
+++ casino/casino-proxy/src/main/java/com/qianyi/casinoproxy/controller/CodeNumChangeController.java
@@ -6,11 +6,13 @@ import com.qianyi.casinocore.model.User;
 import com.qianyi.casinocore.service.CodeNumChangeService;
 import com.qianyi.casinocore.service.GameRecordService;
 import com.qianyi.casinocore.service.UserService;
+import com.qianyi.casinocore.util.CommonConst;
 import com.qianyi.casinocore.vo.CodeNumChangeVo;
 import com.qianyi.casinocore.vo.PageResultVO;
 import com.qianyi.casinoproxy.util.CasinoProxyUtil;
 import com.qianyi.modulecommon.reponse.ResponseEntity;
 import com.qianyi.modulecommon.reponse.ResponseUtil;
+import com.qianyi.modulecommon.util.MessageUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -23,9 +25,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -40,6 +40,23 @@ public class CodeNumChangeController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private MessageUtil messageUtil;
+
+    public static Map<Integer,String> map = new HashMap<>();
+
+    static {
+        map.put(CommonConst.NUMBER_0,"有效投注");
+        map.put(CommonConst.NUMBER_1,"清0点");
+        map.put(CommonConst.NUMBER_2,"充值");
+        map.put(CommonConst.NUMBER_3,"总控上分");
+        map.put(CommonConst.NUMBER_4,"代理上分");
+        map.put(CommonConst.NUMBER_5,"总控人工清零");
+        map.put(CommonConst.NUMBER_6,"每日奖励");
+        map.put(CommonConst.NUMBER_7,"晋级奖励");
+    }
+
     @ApiOperation("查询用户打码明细表")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "pageSize", value = "每页大小(默认10条)", required = false),
@@ -67,6 +84,7 @@ public class CodeNumChangeController {
                             accountChangeVo.setAccount(user.getAccount());
                         }
                     });
+                    accountChangeVo.setTypeString(messageUtil.get(map.get(accountChangeVo.getType())));
                     accountChangeVoList.add(accountChangeVo);
                 });
             }
