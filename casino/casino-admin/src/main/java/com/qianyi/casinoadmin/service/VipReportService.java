47afa8b9f5d721c8dab2a73b7d76fea27775eefe
puff <yinpuff@gmail.com>
2023-01-16 22:58:44 +0800

    vip report bug

--- casino/casino-admin/src/main/java/com/qianyi/casinoadmin/service/VipReportService.java
+++ casino/casino-admin/src/main/java/com/qianyi/casinoadmin/service/VipReportService.java
@@ -66,6 +66,9 @@ public class VipReportService {
         }
         if (StrUtil.isNotBlank(vipReportDTO.getProxyUserName())) {
             ProxyUser proxyUser = proxyUserService.findByUserName(vipReportDTO.getProxyUserName());
+            if(ObjectUtil.isNull(proxyUser)){
+                return PageResult.getPageResult(pageBounds, new LinkedList());
+            }
             Integer proxyRole = proxyUser.getProxyRole();
             LevelAwardVo levelAwardVo = proxyVipMapper.userLevelInfo(proxyUser.getId(), proxyRole,
                     vipReportDTO.getStartTime(), vipReportDTO.getEndTime(), vipReportDTO.getStartDate(), vipReportDTO.getEndDate());
@@ -82,7 +85,7 @@ public class VipReportService {
                 vo.setWinLoss(levelAwardVo.getWinLoss());
             }
             list.add(vo);
-            return PageResult.getPageResult(pageBounds, list);
+            return PageResult.getPageResult(list.size(), pageBounds, list);
         }
         return PageResult.getPageResult(pageBounds, new LinkedList());
     }
@@ -208,6 +211,9 @@ public class VipReportService {
         LevelReportTotalVo levelReportTotalVo;
         if (StrUtil.isNotBlank(vipReportTotalDTO.getProxyUserName())) {
             ProxyUser proxyUser = proxyUserService.findByUserName(vipReportTotalDTO.getProxyUserName());
+            if(ObjectUtil.isNull(proxyUser)){
+                return new LevelReportTotalVo();
+            }
             Integer proxyRole = proxyUser.getProxyRole();
             vipReportTotalDTO.setProxyLevel(proxyRole);
             vipReportTotalDTO.setProxyUserId(proxyUser.getId());
