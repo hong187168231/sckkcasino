579a7f24ff310ebcea01742e12db5b3f215e3e30
puff <yinpuff@gmail.com>
2023-02-16 00:50:59 +0800

    1

--- casino/casino-core/src/main/java/com/qianyi/casinocore/repository/LevelWaterChangeRepository.java
+++ casino/casino-core/src/main/java/com/qianyi/casinocore/repository/LevelWaterChangeRepository.java
@@ -12,7 +12,7 @@ public interface LevelWaterChangeRepository extends JpaRepository<LevelWaterChan
     @Query(value = "select ifnull(sum(lwc.bet_water),0)  from level_water_change lwc WHERE lwc.user_id = ?1 ;",nativeQuery = true)
     BigDecimal findTotalBetWater(Long userId);
 
-    @Query(value = "select ifnull(sum(lwc.bet_water),0)  from level_water_change lwc WHERE lwc.user_id = ?1 and date_sub(curdate(), INTERVAL 30 DAY) <= date(`create_time`) ;",nativeQuery = true)
+    @Query(value = "select ifnull(sum(lwc.bet_water),0)  from level_water_change lwc WHERE lwc.user_id = ?1 and date_sub(curdate(), INTERVAL 10 DAY) <= date(`create_time`) ",nativeQuery = true)
     BigDecimal find10DayBetWater(Long userId);
 
     @Query
