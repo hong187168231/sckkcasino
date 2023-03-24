ebd975ad45844abe382a8faabdf0b9d4bccee004
puff <yinpuff@gmail.com>
2023-03-24 13:39:38 +0800

    1

--- casino/casino-web/src/main/java/com/qianyi/casinoweb/config/ThreadPoolConfig.java
+++ casino/casino-web/src/main/java/com/qianyi/casinoweb/config/ThreadPoolConfig.java
@@ -4,6 +4,8 @@ import com.qianyi.modulecommon.util.Threads;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -18,25 +20,33 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ThreadPoolConfig
 {
     // 核心线程池大小
-    private int corePoolSize = 120;
+//    private int corePoolSize = 50;
+//
+//
+//    /**
+//     * 执行周期性或定时任务
+//     */
+//    @Bean(name = "scheduledExecutorService")
+//    protected ScheduledExecutorService scheduledExecutorService()
+//    {
+//        return new ScheduledThreadPoolExecutor(corePoolSize,
+//                new BasicThreadFactory.Builder().namingPattern("schedule-pool-%d").daemon(true).build(),
+//                new ThreadPoolExecutor.CallerRunsPolicy())
+//        {
+//            @Override
+//            protected void afterExecute(Runnable r, Throwable t)
+//            {
+//                super.afterExecute(r, t);
+//                Threads.printException(r, t);
+//            }
+//        };
+//    }
 
-
-    /**
-     * 执行周期性或定时任务
-     */
-    @Bean(name = "scheduledExecutorService")
-    protected ScheduledExecutorService scheduledExecutorService()
-    {
-        return new ScheduledThreadPoolExecutor(corePoolSize,
-                new BasicThreadFactory.Builder().namingPattern("schedule-pool-%d").daemon(true).build(),
-                new ThreadPoolExecutor.CallerRunsPolicy())
-        {
-            @Override
-            protected void afterExecute(Runnable r, Throwable t)
-            {
-                super.afterExecute(r, t);
-                Threads.printException(r, t);
-            }
-        };
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(30);
+        return taskScheduler;
     }
+
 }
